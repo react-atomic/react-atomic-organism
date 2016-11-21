053306bb80cb5dd714fefa5f11dbd4b001d012d6
Hill <hill@kimo.com>
2016-11-21 13:49:05 +0000

    move Breadcrumb and tab to organism-react-navigation

--- packages/organism-react-navigation/src/index.js
+++ packages/organism-react-navigation/src/index.js
@@ -3,6 +3,9 @@ export DropDown from '../ui/organisms/DropDown';
 export SideMenu from '../ui/organisms/SideMenu';
 export HorizontalToVerticalMenu from '../ui/organisms/HorizontalToVerticalMenu';
 export Pagination from '../ui/organisms/Pagination';
+export Breadcrumb from '../ui/organisms/Breadcrumb';
+export Tab from '../ui/organisms/Tab';
+export TabView from '../ui/organisms/TabView';
 
 // Stores
 export navigationStore from './stores/navigationStore';
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 // ui
-export { default as Breadcrumb } from '../ui/organisms/Breadcrumb';
 export { default as CDN } from '../ui/organisms/cdn';
 export { default as FormattedJson } from '../ui/organisms/formatted_json';
-export { default as Items } from '../ui/organisms/items';
-export { default as Tab } from '../ui/organisms/tab';
-export { default as TabView } from '../ui/organisms/tab_view';
 export { default as Tappable } from '../ui/organisms/tappable';
 
 // card view
