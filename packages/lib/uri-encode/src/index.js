96126c9c4416eb5b66657f3f24dee9feea5b30fb
Hill <hill@kimo.com>
2017-01-26 20:38:06 +0000

    Add uri-encode

---
+++ packages/lib/uri-encode/src/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const uriencode = (uri)=>
+{
+    if (!uri) {
+        return uri;
+    }
+    uri = uri.replace(/#/g,'\uff03');
+    uri = encodeURIComponent(uri)
+        .replace(/%20/g,'+')
+        .replace(/%26/g,'&')
+        .replace(/%5B/g,'[')
+        .replace(/%5D/g,']');
+    return uri;
+}
+
+export default uriencode;
