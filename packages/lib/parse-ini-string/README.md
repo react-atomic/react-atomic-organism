3b6c4419c5a24a7988bc16363b239fd771c60140
Hill <hill@kimo.com>
2017-03-29 17:28:04 +0000

    Add parse-ini-string

---
+++ packages/lib/parse-ini-string/README.md
@@ -0,0 +1,13 @@
+parse-ini-string
+===============
+   * GIT
+      * https://github.com/react-atomic/react-atomic-organism/tree/master/packages/lib/parse-ini-string 
+   * NPM
+      * https://npm.im/parse-ini-string
+
+## PHP
+   * http://php.net/manual/en/function.parse-ini-string.php
+
+## Object
+   * https://npm.im/object-nested
+
---
+++ packages/lib/parse-ini-string/src/index.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const commentReg = /^\s*[;]/;
+const kvReg=/^\[([^\]]*)\]$|^([^=]+)(=(.*))?$/i;
+const linesReg=/[\r\n]+/g;
+const trimReg=/(^\s+)|\s+$/g;
+const isArray = Array.isArray;
+
+const trim = (s) =>
+{
+    return s.replace(trimReg, '');
+}
+
+const isQuoted = (s) =>
+{
+  if (s.length<=1) {
+        return false;
+  }
+  return (s.charAt(0) === '"' && s.slice(-1) === '"') ||
+    (s.charAt(0) === "'" && s.slice(-1) === "'")
+}
+
+const stripQuote = (s) =>
+{
+   return s.substring(1, s.length - 1);
+}
+
+const isMultiLine = (s) =>
+{
+    let n =trim(s);
+    if (isQuoted(n)) {
+        return false;
+    }
+    if (n.charAt(0) === '"') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const parse = (s) =>
+{
+    const lines = s.split(linesReg);
+    let value = '';
+    let key = '';
+    let p = {};
+    lines.forEach((line)=>{
+        let isEnd = false;
+        if (!line || commentReg.test(line)) {
+            return;
+        }
+        if (!key) {
+            const match = line.match(kvReg);
+            key = match[2];
+            value = value = match[3] ? (match[4] || '') : '';
+            if (key.length > 2 && key.slice(-2) === '[]') {
+                  key = key.substring(0, key.length - 2)
+                  if (!p[key]) {
+                    p[key] = []
+                  } else if (!Array.isArray(p[key])) {
+                    p[key] = [p[key]]
+                  }
+            }
+            if (!isMultiLine(value)) {
+                isEnd = true;
+            }
+            switch (value) {
+              case 'true':
+              case 'false':
+              case 'null': value = JSON.parse(value)
+            }
+        } else {
+            if (trim(line).slice(-1) === '"') {
+                isEnd = true;
+            }
+            value+=line;
+        }
+        if (isEnd) {
+            if (value.length && isQuoted(value)) {
+                value = stripQuote(value);
+            }
+            if (isArray(p[key])) {
+              p[key].push(value);
+            } else {
+              p[key] = value;
+            } 
+            key = '';
+            value = '';
+        }
+    });
+    return p;
+};
+
+export default parse;
