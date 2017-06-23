104c3ffab957457dcb370a325c2b1a2e0d07d063
Hill <hill@kimo.com>
2017-06-23 07:25:09 +0000

    fixed dropdown unexpected space.(hanout)

--- packages/organism-react-navigation/ui/organisms/DropDown.jsx
+++ packages/organism-react-navigation/ui/organisms/DropDown.jsx
@@ -1,16 +1,24 @@
-import React from 'react'; 
-
+import React, {cloneElement} from 'react'; 
+import get from 'get-object-value';
 import {
     Icon,
     Menu,
     Item
 } from 'react-atomic-molecule';
-
 import DropDownIcon from 'ricon/DropDown';
 
-const DropDown = (props) =>
+const DropDown = ({list, children, ...props}) =>
 {
-    const {list, children } = props;
+    const listStyle = get(list, ['props', 'style']);
+    list = cloneElement(
+        list,
+        {
+            style: {
+                ...listStyle,
+                marginTop: -10, //aviod not expected hover out
+            }
+        }
+    );
     return (
         <Menu style={Styles.container} className="compact">
             <Item style={Styles.tip} className="simple dropdown">
