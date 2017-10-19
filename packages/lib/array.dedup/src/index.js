e5cd796ff16f276750473e96776e245612be1b58
Hill <hill@kimo.com>
2017-10-19 14:15:44 +0000

    Add failback

--- packages/lib/array.dedup/src/index.js
+++ packages/lib/array.dedup/src/index.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const dedup = (array) => 
-    array.filter(
+const dedup = (array) => {
+    if (!array || !array.filter) {
+        return array;
+    }
+    return array.filter(
         (item, pos, arr) => arr.indexOf(item) === pos
     );
+}
 
 export default dedup;
