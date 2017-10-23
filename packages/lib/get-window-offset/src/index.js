7b958020902982c4e82676ecde62ad77176608a5
Hill <hill@kimo.com>
2017-10-23 18:28:07 +0000

    export isOnScreen

--- packages/lib/get-window-offset/src/index.js
+++ packages/lib/get-window-offset/src/index.js
@@ -2,3 +2,4 @@ export default from './getWindowOffset';
 
 export { default as alignUI} from './alignUI';
 export { default as getPositionString} from './getPositionString';
+export { default as isOnScreen } from './isOnScreen';
