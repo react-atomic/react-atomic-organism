72e7c33744465156df83a68d8350b33b61a350cf
Hill <hill@kimo.com>
2017-12-14 08:46:20 +0000

    Add export class

--- packages/organism-react-spotlight/src/index.js
+++ packages/organism-react-spotlight/src/index.js
@@ -1,1 +1,7 @@
 export {default} from '../ui/organisms/Spotlight';
+
+// library
+export { default as cleanClass } from './cleanClass';
+export { default as cleanZIndex } from './cleanZIndex';
+export { default as setClass } from './setClass';
+export { default as showEl } from './showEl';
