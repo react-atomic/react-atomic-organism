cf6fef73a6629a86495e8a04f889c9d775a39325
Hill <hill@kimo.com>
2018-03-20 23:36:27 +0800

    improve performance

--- packages/organism-react-parallax/ui/organisms/ParallaxBackgroundImage.jsx
+++ packages/organism-react-parallax/ui/organisms/ParallaxBackgroundImage.jsx
@@ -1,20 +1,87 @@
 import React, {Component} from 'react';
-import {
-    scrollDispatch,
-    scrollStore,
-    ScrollSpy,
-    ScrollReceiver,
-} from 'organism-react-scroll-nav';
+import { ScrollInfo } from 'organism-react-scroll-nav';
 import {reactStyle, SemanticUI} from 'react-atomic-molecule';
 import getScrollInfo from 'get-scroll-info';
 import getOffset from 'getoffset';
 import get from 'get-object-value';
 import merge from 'array.merge';
-import easeInOutQuad from 'easing-lib/easeOutQuint';
-scrollDispatch({scrollDelay: 0});
+import aniCal from 'easing-lib/easeOutQuart';
 
 class Content extends Component
 {
+    calOffset(el, props)
+    {
+        if (!props) {
+            props = this.props;
+        }
+        const {speed, targetInfo} = props; 
+        const scrollInfo = this.scrollInfo; 
+        const winH = get(scrollInfo, ['scrollNodeHeight'], 0);
+        const winTop = get(scrollInfo, ['top'], 0);
+        let offset;
+        if (el) {
+            offset = getOffset(el);
+        } else {
+            offset = targetInfo;
+        }
+        if (!offset) {
+            return;
+        }
+        const elViewTop = get(offset, ['top'], 0) - winTop;
+        const elH = get(offset, ['h']);
+        const scrollDist = (speed * (elH + winH)) / 2;
+        const viewCenter = 1 - 2 * (winH - elViewTop) / (winH + elH);
+        const posY = Math.round((scrollDist * viewCenter) - elViewTop);
+        return {posY};
+    }
+
+    handleScroll = nextProps =>
+    {
+        const {targetInfo} = nextProps;
+        if (!targetInfo.isOnScreen) {
+	    return;
+	}
+        const {posY} = get(this.calOffset(null, nextProps), null, {});
+        if (!posY) {
+            return;
+        }
+        this.posY = posY;
+        this.handleScrollAni(posY);
+    }
+
+    handleScrollAni = () =>
+    {
+	let beginTimeStamp = 0;
+	const runScrollAni = timeStamp =>
+	{
+            beginTimeStamp = beginTimeStamp || timeStamp;
+	    const elapsedTime = timeStamp - beginTimeStamp;
+            this.setState(({posY})=>{
+                const go = this.posY - posY;
+                const duration = 1000;
+		let progress = aniCal (
+		    elapsedTime,
+		    this.posY,
+		    go,
+                    duration 
+		);
+                progress = Math.round(progress);
+                if ( elapsedTime < duration && progress !== this.posY) {
+                    requestAnimationFrame(runScrollAni);
+                }
+		return { posY: progress };
+	    });
+	}
+	requestAnimationFrame(runScrollAni);
+    }
+
+    handleResize = () =>
+    {
+        this.scrollInfo = getScrollInfo();
+        const {posY} = this.calOffset(this.el);
+        this.setState(()=>{return { posY }});
+    }
+
     constructor(props)
     {
         super(props);
@@ -25,118 +92,16 @@ class Content extends Component
 
     shouldComponentUpdate(nextProps, nextState)
     {
-        const {
-            targetInfo,
-        } = nextProps;
+        const { targetInfo, } = nextProps;
+        const { posY } = nextState;
+        if (posY === this.state.posY) {
+            return false;
+        }
         if (!targetInfo.isOnScreen) {
-            this.scrollInfo = null;
             return false;
         } else {
             return true;
         }
-    }
-
-    calOffset(el, props)
-    {
-        if (!props) {
-            props = this.props;
-        }
-        const {speed, targetInfo} = props; 
-        const scrollInfo = this.scrollInfo; 
-        const winW = get(scrollInfo, ['scrollNodeWidth'], 0);
-        const winH = get(scrollInfo, ['scrollNodeHeight'], 0);
-        const winTop = get(scrollInfo, ['top'], 0);
-        let offset;
-        if (targetInfo.targetId) {
-           offset = scrollStore.getOffset(targetInfo.targetId); 
-        } else {
-            if (el) {
-                offset = getOffset(el);
-            } else {
-                return;
-            }
-        }
-        const elViewTop = get(offset, ['top'], 0) - winTop;
-        const elH = get(offset, ['h']);
-        const scrollDist = (speed * (elH + winH)) / 2;
-        const resultH = elH + Math.abs(winH - elH) * (1 - speed);
-        const viewCenter = 1 - 2 * (winH - elViewTop) / (winH + elH);
-        const posY = (scrollDist * viewCenter) - elViewTop;
-        return {posY, resultH};
-    }
-
-    handleResize = () =>
-    {
-        const scrollInfo = getScrollInfo();
-        this.scrollInfo = scrollInfo;
-        const winW = get(scrollInfo, ['scrollNodeWidth'], 0);
-        const winH = get(scrollInfo, ['scrollNodeHeight'], 0);
-        const {posY, resultH} = this.calOffset(this.el);
-        this.setState(()=>{return {
-            winW,
-            winH,
-            posY,
-            resultH
-        }});
-    }
-
-    handleScroll = (nextProps) =>
-    {
-	if (this.scrollTimer) {
-            clearTimeout(this.scrollTimer);
-	}
-        const {targetInfo} = nextProps;
-        if (!targetInfo.isOnScreen) {
-	    return;
-	}
-        const {posY, resultH} = this.calOffset(null, nextProps);
-        if (this.isRun) {
-            this.scrollTimer = setTimeout(()=>{
-                this.handleScroll(nextProps); 
-            },150); 
-            return;
-        }
-        this.handleScrollAni(posY, this.lastY, resultH);
-        this.lastY = posY;
-    }
-
-
-    handleScrollAni = (lastY, oldLastY, resultH) =>
-    {
-	if (this.aniTimer) {
-            clearTimeout(this.aniTimer);
-	}
-	if (this.isRun) {
-	    this.aniTimer = setTimeout(()=>{
-		this.handleScrollAni(lastY, oldLastY);
-	    }, 150);
-	    return;
-	}
-	let beginTimeStamp = null;
-        oldLastY = oldLastY || 0;
-	const go = lastY - oldLastY;
-	const duration = 350;
-	const runScrollAni = (timeStamp) =>
-	{
-	    this.isRun = true;
-	    if (!beginTimeStamp) {
-		beginTimeStamp = timeStamp;
-	    }
-	    const elapsedTime = timeStamp - beginTimeStamp;
-	    const progress = easeInOutQuad(
-		elapsedTime,
-		oldLastY,
-		go,
-		duration
-	    );
-	    this.setState({posY: progress, resultH});
-	    if ( elapsedTime < duration ) {
-	    	requestAnimationFrame(runScrollAni);
-	    } else {
-		this.isRun = false;
-	    }
-	}
-	requestAnimationFrame(runScrollAni);
     }
 
     componentDidMount()
@@ -158,9 +123,10 @@ class Content extends Component
 
     componentWillReceiveProps(next)
     {
-        const scrollInfo = getScrollInfo();
+        const scrollInfo = get(next, ['targetInfo', 'scrollInfo']);
         if ( !this.scrollInfo || 
-            this.scrollInfo.top !== scrollInfo.top ) {
+            this.scrollInfo.top !== scrollInfo.top
+        ) {
              this.scrollInfo = scrollInfo;
              this.handleScroll(next);
         }
@@ -178,16 +144,7 @@ class Content extends Component
             refCb,
             ...others
         } = this.props;
-        const {
-            posY,
-            resultH,
-            winW,
-            winH
-        } = this.state;
-        let height = resultH;
-        if (!height) {
-            height = winH;
-        }
+        const { posY } = this.state;
         return (
             <SemanticUI
                 {...others}
@@ -203,9 +160,7 @@ class Content extends Component
                 {children}
                 <SemanticUI
                     className="parllax-background"
-                    styles={reactStyle({
-                        ...Styles.background,
-                    }, false, false)}
+                    style={Styles.background}
                 >
                     <SemanticUI className="parllax-layer"
                         styles={reactStyle({
@@ -214,14 +169,10 @@ class Content extends Component
                         },false,false)}
                     >
                         <SemanticUI className="parllax-image"
-                            styles={reactStyle({
+                            style={{
                                 ...Styles.backgroundImage,
-                                //backgroundSize: `${winW}px ${height}px`,
-                                backgroundSize: `cover`,
                                 backgroundImage: 'url("'+backgroundImage+'")',
-                                width: winW,
-                                height
-                            }, false, false)}
+                            }}
                         />
                     </SemanticUI>
                 </SemanticUI>
@@ -230,14 +181,12 @@ class Content extends Component
     }
 }
 
-const ParallaxBackgroundImage = props =>
-<ScrollSpy {...props}>
-    <ScrollReceiver />
-</ScrollSpy>
+const ParallaxBackgroundImage = props => <ScrollInfo {...props} />;
 
 ParallaxBackgroundImage.defaultProps = {
     container: <Content />,
-    speed: 0.6,
+    noDelay: true,
+    speed: 0.5,
 };
 
 export default ParallaxBackgroundImage;
@@ -252,6 +201,7 @@ const Styles = {
         top: 0,
         left: 0,
         zIndex: -1,
+        willChange: 'scroll-position',
     },
     backgroundLayer: {
         position: 'relative',
@@ -260,5 +210,9 @@ const Styles = {
     backgroundImage: {
         backgroundPosition: '50% 50%',
         backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        width: '100vw',
+        height: '100vh',
+        willChange: 'scroll-position',
     }
 };
