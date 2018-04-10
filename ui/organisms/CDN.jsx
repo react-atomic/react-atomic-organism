1e8cf5d3b071fb9d1201ffd1f2a0818a26dbf23e
Hill <hill@kimo.com>
2018-04-10 15:13:25 +0000

    update cdn

--- ui/organisms/CDN.jsx
+++ ui/organisms/CDN.jsx
@@ -1,31 +1,30 @@
-import React, {Component} from 'react'; 
+import React, {PureComponent} from 'react'; 
 import {
     Image
 } from 'react-atomic-molecule';
 const keys  = Object.keys;
 
-export default class CDN extends Component
+const stripHttpReg = /^(http)?(s)?(\:)?(\/\/)/gi;
+
+export default class CDN extends PureComponent
 {
     render()
     {
-        let props = {...this.props}; 
-        let propsKeys = keys(props); 
-        let key;
-        let cdnProps=[];
-        for (let i=0,len=propsKeys.length; i<len; i++)
-        {
-            key = propsKeys[i];
+        const {src, ...props} = this.props;
+        const cdnProps = {};
+        keys(props).forEach( key => {
             if ('cdn' === key.substr(0,3)) {
-                cdnProps[key.substr(4)] = this.props[key];
+                cdnProps[key.substr(4)] = props[key];
                 delete props[key];
             }
-        }
-        let query = cdnProps.map((k,v)=>k+'='+encodeURIComponent(v)).join('&');
-        let link = this.props.src.replace(/^(http)?(s)?(\:)?(\/\/)/gi,'');
-        let url = '//i2.wp.com/'+ link;
-        let src = url+ '?'+ query;
+        });
+        const query = keys(cdnProps).
+            map( key => key+ '='+ encodeURIComponent(cdnProps[key])).
+            join('&');
+        const link = src.replace(stripHttpReg, '');
+        const url = '//i2.wp.com/'+ link+ '?'+ query;
         return (
-          <Image {...props} style={{margin: '0 auto'}} src={src} />
+          <Image {...props} style={{margin: '0 auto'}} src={url} />
         );
     }
 }
