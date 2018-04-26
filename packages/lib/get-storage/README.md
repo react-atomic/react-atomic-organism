52b158936effdc7dda1b4ce35bd82bf64cb3e7e0
Hill <hill@kimo.com>
2018-04-26 16:05:28 +0800

    Add get-storage

---
+++ packages/lib/get-storage/README.md
@@ -0,0 +1,12 @@
+Get Storage
+===============
+   * GIT
+      * https://github.com/react-atomic/react-atomic-organism/tree/master/packages/lib/get-storage
+   * NPM
+      * https://npm.im/get-storage
+
+## How to use
+```
+```
+
+
---
+++ packages/lib/get-storage/src/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import get from 'get-object-value';
+
+const storage = storageType => key => value =>
+{
+    if ('undefined' === typeof window) {
+        return;
+    }
+    const win = window;
+    const store = get(win, [storageType]);
+    if ('undefined' === value) {
+        return store.getItem(key);
+    } else {
+        return store.setItem(key, value);
+    }
+}
+
+const localStorage = storage('localStorage');
+
+const sessionStorage = storage('sessionStorage');
+
+export {
+    localStorage,
+    sessionStorage
+};
