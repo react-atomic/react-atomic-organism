0bd8832029a1a9fbaf23fe361fb456163aede21e
Hill <hill@kimo.com>
2018-05-23 16:58:39 +0800

    Add IframeContainer

--- packages/organism-react-iframe/src/index.js
+++ packages/organism-react-iframe/src/index.js
@@ -1,1 +1,2 @@
 export default from '../ui/organisms/Iframe';
+export {default as IframeContainer} from '../ui/organisms/IframeContainer';
---
+++ packages/organism-react-iframe/ui/organisms/IframeContainer.jsx
@@ -0,0 +1,56 @@
+import React, { PureComponent } from "react"
+import get from "get-object-value"
+import { SemanticUI } from "react-atomic-molecule"
+
+class IframeContainer extends PureComponent {
+  state = { iframeH: "auto" }
+
+  handleMessage = e => {
+    let data = e.data
+    if ("string" === typeof data) {
+      try {
+        data = JSON.parse(get(data, null, "{}"))
+      } catch (ex) {}
+    }
+    const { type, h } = data
+    if (-1 !== `|${type}|`.indexOf('|iframeH|')) {
+      this.setState({
+        iframeH: h + 50,
+      })
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("message", this.handleMessage, false)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("message", this.handleMessage, false)
+  }
+
+  render() {
+    const { iframeH } = this.state
+    const { src } = this.props
+    return (
+      <SemanticUI
+        style={{
+          ...Styles.iframe,
+          height: iframeH,
+        }}
+        atom="iframe"
+        refCb={el => (this.iframe = el)}
+        onLoad={this.handleLoad}
+        src={src}
+      />
+    )
+  }
+}
+
+export default IframeContainer
+
+const Styles = {
+  iframe: {
+    width: "100%",
+    border: 0,
+  },
+}
