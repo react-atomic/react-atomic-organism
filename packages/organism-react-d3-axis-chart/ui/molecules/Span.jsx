114e2a966d779ddd9b589b4eeac62fe2675d4401
Hill <hill@kimo.com>
2018-06-11 13:36:19 +0000

    Export Span and Text

---
+++ packages/organism-react-d3-axis-chart/ui/molecules/Span.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+import { SemanticUI } from 'react-atomic-molecule';
+
+const Span = props =>
+{
+    return <SemanticUI {...props} />;
+}
+
+Span.defaultProps = {
+    atom: 'tspan'
+};
+
+export default Span;
