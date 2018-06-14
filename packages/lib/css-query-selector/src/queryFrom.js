e2d47f60ae1709ab5f32ec2ee8502e9875375999
Hill <hill@kimo.com>
2018-06-14 18:11:32 +0800

    Add query from

---
+++ packages/lib/css-query-selector/src/queryFrom.js
@@ -0,0 +1,41 @@
+const queryFrom = base =>
+{
+
+    if ('function' !== typeof base) {
+        console.error('query base should pass with function');
+    }
+
+    const doc = base;
+
+    const queryOne = sel => doc().querySelector(sel);
+
+    const queryAll = sel => Array.from(doc().querySelectorAll(sel));
+
+    const queryEl = el => ('string' === typeof el) ? queryOne(el) : el;
+
+    const queryAncestor = (el, ancestor) =>
+    {
+        el = queryEl(el);
+        let hit = false;
+
+        queryAll(ancestor).some(
+            p => {
+                if (p.contains(el)) {
+                    hit = p;
+                    return true;
+                } else {
+                    return false;
+                }   
+            }   
+        );  
+        return hit;
+    }
+    return {
+        queryOne,
+        queryAll,
+        queryEl,
+        queryAncestor
+    }
+}
+
+export default queryForm;
