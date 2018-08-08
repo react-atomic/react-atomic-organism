a5c8d9d154177d893ae842471d4e1f7c00de8e30
Hill <hill@kimo.com>
2018-08-08 02:55:53 +0000

    uml component wip

--- packages/organism-react-graph/ui/organisms/Line.jsx
+++ packages/organism-react-graph/ui/organisms/Line.jsx
@@ -1,13 +1,30 @@
 import React, {PureComponent} from 'react'
 import {SemanticUI} from 'react-atomic-molecule'
+import {line} from 'd3-lib';
 
 class Line extends PureComponent
 {
     render()
     {
-       const props = this.props
+       const {start, end, svgLine, curve, ...props} = this.props
+        const params = {};
+        if (start && end) {
+           if (svgLine) {
+             params.x1 = start.x;
+             params.y1 = start.y;
+             params.x2 = end.x;
+             params.y2 = end.y;
+           } else {
+             params.d = line(start, end, curve)
+           }
+        }
+        params.atom = (svgLine) ? 'line': 'path'
        return (
-            <SemanticUI atom="line" ui={false} {...props} />
+            <SemanticUI 
+                ui={false}
+                {...props}
+                {...params}
+            />
        )
     }
 }
