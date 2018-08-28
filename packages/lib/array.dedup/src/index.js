669f580b239571aeefcc1f8cd5650b31b72de9a8
Hill <hill@kimo.com>
2018-08-28 04:01:05 +0000

    arrange code

--- packages/lib/array.dedup/src/index.js
+++ packages/lib/array.dedup/src/index.js
@@ -1,12 +1,10 @@
-'use strict';
-
-const dedup = (array) => {
+const dedup = array => {
     if (!array || !array.filter) {
-        return array;
+        return array
     }
-    return array.filter(
+    return array.filter (
         (item, pos, arr) => arr.indexOf(item) === pos
-    );
+    )
 }
 
-export default dedup;
+export default dedup
