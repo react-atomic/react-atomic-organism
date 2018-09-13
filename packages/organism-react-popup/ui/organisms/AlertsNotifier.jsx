645056e35dc26ee1f2742612a4cadadd9143dc53
Hill <hill@kimo.com>
2018-09-13 21:06:35 +0000

    update alert component

--- packages/organism-react-popup/ui/organisms/AlertsNotifier.jsx
+++ packages/organism-react-popup/ui/organisms/AlertsNotifier.jsx
@@ -1,145 +1,143 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import {Set} from 'immutable';
 import Animate from 'organism-react-animate';
 import XIcon from 'ricon/X';
-import React, {PureComponent} from 'react'; 
-import PropTypes from 'prop-types';
 import {Message, reactStyle} from 'react-atomic-molecule';
-import {Set} from 'immutable';
 
-class XIconEl extends PureComponent
-{
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-           hoverStyle: null 
-        };
-    }
+const messageTypes = ['success', 'info', 'warning', 'error']
 
-    handleMouseEnter = ()=>
-    {
-        this.setState({
-            hoverStyle: {
-                opacity: '.9'
-            } 
-        });
-    }
+class XIconEl extends PureComponent {
+  state = {
+    hoverStyle: null,
+  };
 
-    handleMouseLeave = ()=>
-    {
-        this.setState({
-            hoverStyle: null 
-        });
-    }
+  handleMouseEnter = () => {
+    this.setState({
+      hoverStyle: {
+        opacity: '.9',
+      },
+    });
+  };
 
-    render()
-    {
-        const props = this.props;
-        const {hoverStyle} = this.state;
-        return (
-            <XIcon 
-                style={{
-                    ...Styles.xicon,
-                    ...hoverStyle
-                }}
-                weight=".1rem"
-                onMouseEnter={this.handleMouseEnter}
-                onMouseLeave={this.handleMouseLeave}
-                {...props}
-            />
-        );
-    }
+  handleMouseLeave = () => {
+    this.setState({
+      hoverStyle: null,
+    });
+  };
+
+  render() {
+    const props = this.props;
+    const {hoverStyle} = this.state;
+    return (
+      <XIcon
+        style={{
+          ...Styles.xicon,
+          ...hoverStyle,
+        }}
+        weight=".1rem"
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+        {...props}
+      />
+    );
+  }
 }
 
-class AlertsNotifier extends PureComponent
-{
+class AlertsNotifier extends PureComponent {
+  state = {
+      dismissedAlerts: Set(),
+  }
 
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-           dismissedAlerts: Set() 
-        };
+  static getDerivedStateFromProps(nextProps, prevState)
+  {
+    const {alerts} = nextProps
+    if (alerts !== prevState.prevPropsAlerts) {
+      return {
+        prevPropsAlerts: alerts,
+        dismissedAlerts: Set()
+      }
+    } else {
+      return null
     }
+  }
 
-    dismiss(item)
-    {
-        if (this.props.onDismiss) {
-            //if callback specified, call it
-            this.props.onDismiss(item);
-        }
-        //if no callback for dismissal, just update our state
-        this.setState({ dismissedAlerts: this.state.dismissedAlerts.add(item) });
+  dismiss(item) {
+    const {onDismiss} = this.props
+    if ('function' === typeof onDismiss) {
+      onDismiss(item);
     }
+    //if no callback for dismissal, just update our state
+    this.setState(({dismissedAlerts})=>{
+      return {
+        dismissedAlerts: dismissedAlerts.add(item)
+      }
+    })
+  }
 
-    render()
-    {
-        const {
-            ani,
-            alerts,
-            position
-        } = this.props;
-        const {
-            dismissedAlerts
-        } = this.state;
-        let alertArr = [];
-        alerts.forEach((item, key)=>{
-            if ( !dismissedAlerts.has(item) ) {
-                if (["success", "info", "warning", "error"].indexOf(item.type) < 0) {
-                        item.type = "info";
-                }
-                alertArr.push((
-                    <Message key={key} messageType={item.type} header={item.header}>
-                        {item.message}
-                        <XIconEl 
-                            onClick={this.dismiss.bind(this, item)}
-                        />
-                    </Message>
-                ));
-            }
-        });
-        let positionStyle = {};
-        if ('top' === position) {
-            positionStyle.top = 5;
-        } else {
-            positionStyle.bottom = 5;
+  render() {
+    const {ani, alerts, position} = this.props;
+    const {dismissedAlerts} = this.state;
+    if (!alerts.length) {
+      return null
+    }
+    const alertArr = [];
+    alerts.forEach((item, key) => {
+      const thisItem = ('string' === typeof item) ?
+        {message: item} :
+        item;
+      if (!dismissedAlerts.has(item)) {
+        if (-1 === messageTypes.indexOf(thisItem.type)) {
+          thisItem.type = 'info';
         }
-        return (
-            <Animate
-                {...ani}
-                style={{...Styles.container, ...positionStyle}}
-            >
-                {alertArr}
-            </Animate>
+        alertArr.push(
+          <Message key={key} messageType={thisItem.type} header={thisItem.header}>
+            {thisItem.message}
+            <XIconEl onClick={this.dismiss.bind(this, item)} />
+          </Message>
         );
+      }
+    });
+    const positionStyle = {};
+    if ('top' === position) {
+      positionStyle.top = 5;
+    } else {
+      positionStyle.bottom = 5;
     }
+    return (
+      <Animate {...ani} style={{...Styles.container, ...positionStyle}}>
+        {alertArr}
+      </Animate>
+    );
+  }
 }
 
 AlertsNotifier.propTypes = {
-    alerts: PropTypes.array.isRequired,
-    onDismiss: PropTypes.func
+  alerts: PropTypes.array,
+  onDismiss: PropTypes.func,
 };
 
 AlertsNotifier.defaultProps = {
-    ani: {
-        appear: 'fadeIn',
-        enter: 'fadeIn',
-        leave: 'fadeOut'
-    },
-    position: 'top' 
+  ani: {
+    appear: 'fadeIn',
+    enter: 'fadeIn',
+    leave: 'fadeOut',
+  },
+  position: 'top',
 };
 
 export default AlertsNotifier;
 
 const Styles = {
-    container: {
-        position: 'fixed',
-        left: 10,
-        right: 10,
-        zIndex: 9999
-    },
-    xicon: {
-        top: 20,
-        right: 10,
-        opacity: '.5'
-    }
+  container: {
+    position: 'fixed',
+    left: 10,
+    right: 10,
+    zIndex: 9999,
+  },
+  xicon: {
+    top: 20,
+    right: 10,
+    opacity: '.5',
+  },
 };
