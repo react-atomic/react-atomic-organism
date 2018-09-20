6034f6cab9ad410b3571429adde8d1b214f25a0f
Hill <hill@kimo.com>
2018-09-21 07:56:00 +0800

    add Storage class

---
+++ packages/lib/get-storage/src/Storage.js
@@ -0,0 +1,45 @@
+const keys = Object.keys;
+const toInt = d => parseInt(d, 10);
+
+class Storage {
+  constructor(_storage) {
+    this._storage = _storage;
+  }
+
+  set(k, v) {
+    const _v = JSON.stringify(v);
+    const vLen = _v.length;
+    const s = vLen + ',' + _v;
+    this._storage(k)(s);
+    return new Storage(this._storage);
+  }
+
+  merge(arr) {
+    if (!arr || 'object' !== typeof arr) {
+      return this;
+    }
+    const pKeys = keys(arr);
+    if (!pKeys || !pKeys.length) {
+      return this;
+    }
+    pKeys.forEach(key => this.set(key, arr[key]));
+    return new Storage(this._storage);
+  }
+
+  get(k) {
+    const s = this._storage(k)();
+    if (!s) {
+      return undefined;
+    }
+    const iStar = s.indexOf(',');
+    const vLen = toInt(s.substring(0, iStar));
+    const value = s.substr(iStar + 1);
+    if (vLen === value.length) {
+      return JSON.parse(value);
+    } else {
+      return undefined;
+    }
+  }
+}
+
+export default Storage;
--- packages/lib/get-storage/src/index.js
+++ packages/lib/get-storage/src/index.js
@@ -1,30 +1,25 @@
-'use strict';
-
 import get from 'get-object-value';
+import Storage from './Storage';
 
-const storage = storageType => key => value =>
-{
-    if ('undefined' === typeof window) {
-        return;
-    }
-    const win = window;
-    const store = get(win, [storageType]);
-    if ('undefined' === typeof store) {
-        console.warn('window not support. ['+storageType+']');
-        return;
-    }
-    if ('undefined' === typeof value) {
-        return store.getItem(key);
-    } else {
-        return store.setItem(key, value);
-    }
-}
-
-const localStorage = storage('localStorage');
-
-const sessionStorage = storage('sessionStorage');
-
-export {
-    localStorage,
-    sessionStorage
+const getStorage = storageType => key => value => {
+  if ('undefined' === typeof window) {
+    return;
+  }
+  const win = window;
+  const store = get(win, [storageType]);
+  if ('undefined' === typeof store) {
+    console.warn('window not support. [' + storageType + ']');
+    return;
+  }
+  if ('undefined' === typeof value) {
+    return store.getItem(key);
+  } else {
+    return store.setItem(key, value);
+  }
 };
+
+const localStorage = getStorage('localStorage');
+
+const sessionStorage = getStorage('sessionStorage');
+
+export {Storage, localStorage, sessionStorage};
