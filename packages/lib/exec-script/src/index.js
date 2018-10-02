2dae2f23259d4d8c23f843a40a0ce27aec6d217e
Hill <hill@kimo.com>
2018-10-02 18:38:06 +0800

    Add unit test

--- packages/lib/exec-script/src/index.js
+++ packages/lib/exec-script/src/index.js
@@ -4,31 +4,38 @@ let scriptCount = 0;
 let inlineScripts = [];
 let queueScripts = {};
 
-const keys = Object.keys
+const keys = Object.keys;
 
-const handleScriptOnload = win => i => {
+const handleScriptOnload = (win, errorCb) => i => {
   if (i) {
     delete queueScripts[i];
   }
   if (!keys(queueScripts).length) {
-    inlineScripts.forEach((script, key) =>
-      win.eval('(function(){ '+script+' }())')
-    );
+    inlineScripts.forEach((script, key) => {
+      try {
+        win.eval('(function(){ ' + script + ' }())');
+      } catch (e) {
+        if ('function' === typeof errorCb) {
+          errorCb(e, script);
+        } else {
+          console.error(e.message, script);
+        }
+      }
+    });
     inlineScripts = [];
   }
 };
 
-const execScript = (el, win, jsBase) => {
+const execScript = (el, win, jsBase, errorCb) => {
   if (!win) {
     win = window;
   }
   if (!jsBase) {
     jsBase = document.body;
   }
-  const thisEl = ('string' === typeof el) ?
-    create('div')()({innerHTML:el+''}) :
-    el
-  const onLoad = handleScriptOnload(win);
+  const thisEl =
+    'string' === typeof el ? create('div')()({innerHTML: el + ''}) : el;
+  const onLoad = handleScriptOnload(win, errorCb);
   const scripts = thisEl.getElementsByTagName('script');
   for (let i = 0, len = scripts.length; i < len; i++) {
     const script = scripts[i];
--- packages/lib/exec-script/tests/test.js
+++ packages/lib/exec-script/tests/test.js
@@ -1,14 +1,27 @@
 import {expect} from 'chai';
+import gJsdom from 'jsdom-global';
 
 import exec from '../cjs/src/index.js';
 
-let cmd = ''
+describe('Exec Script', () => {
+  let jsdom;
+  before(() => {
+    jsdom = gJsdom('', {runScripts: 'outside-only'});
+  });
 
-window.eval = s => cmd = s 
+  it('simple', () => {
+    exec('<script>window.a = 1</script>');
+    expect(window.a).to.equal(1);
+  });
 
-describe('Exec Script', ()=>{
-    it('simple', ()=>{
-       exec('<script>window.a = 1</script>')
-       expect(cmd).to.equal('(function(){ window.a = 1 }())')
+  it('syntax error', done => {
+    exec('<script>window.a = (</script>', null, null, (err, js)=>{
+      expect(err.message).to.have.string('Unexpected token');
+      done();
     });
+  });
+
+  after(() => {
+    jsdom();
+  });
 });
