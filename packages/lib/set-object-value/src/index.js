52d3e4e650df760904f9ed4fc8a2b7b12086db60
Hill <hill@kimo.com>
2018-10-03 21:29:46 +0800

    Add append

--- packages/lib/set-object-value/src/index.js
+++ packages/lib/set-object-value/src/index.js
@@ -1,13 +1,19 @@
 'use strict';
 
-const replaceValue = (obj, arrKey, val) =>
+const replaceValue = (obj, arrKey, val, isAppend) =>
 {
     let last = arrKey.pop();
     arrKey.forEach((k)=>{
         obj[k] = obj[k] || {};
         obj = obj[k];
     });
-    obj[last] = val;
+    if (isAppend && !obj[last]) {
+      obj[last] = [val];
+    } else if (isAppend && obj[last].push) {
+      obj[last].push(val);
+    } else {
+      obj[last] = val;
+    }
 }
 
 export default replaceValue;
