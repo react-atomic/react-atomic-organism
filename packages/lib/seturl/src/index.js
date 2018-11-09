5d9cdd2745b6043f570784e0b8ce247ac5d0ce6c
Hill <hill@kimo.com>
2018-11-09 20:54:10 +0800

    Add getPath

--- packages/lib/seturl/src/index.js
+++ packages/lib/seturl/src/index.js
@@ -2,50 +2,40 @@
 
 import getKeyReg from './getKeyReg';
 
-const resetUrl = (url) =>
-{
-    if (!url) {
-        return document.URL;
-    } else {
-        return url;
-    }
-}
+const uriReg = /^(((([^:\/#\?]+:)?(?:(\/\/)((?:(([^:@\/#\?]+)(?:\:([^:@\/#\?]+))?)@)?(([^:\/#\?\]\[]+|\[[^\/\]@#?]+\])(?:\:([0-9]+))?))?)?)?((\/?(?:[^\/\?#]+\/+)*)([^\?#]*)))?(\?[^#]+)?)(#.*)?/;
 
-const getUrl = (key, url) =>
-{
-    url = resetUrl(url);
-    const reg = getKeyReg(key);
-    const exec = reg.exec(url);
-    return (!exec) ?
-        '':
-        decodeURIComponent(exec[3]); 
-}
+const getPath = url => uriReg.exec(url)[2]
 
-const unsetUrl = (key, url) =>
-{
-    const reg = getKeyReg(key);
-    url = resetUrl(url);
-    const exec = reg.exec(url);
-    if (exec) {
-        url = (exec[2]==='?') ? 
-            url.replace(reg,'?'):
-            url.replace(reg,'');
-    }
-    return url;
-}
+const resetUrl = url => (url ? url : document.URL);
 
-const setUrl = ( key, value, url, KeepRawValue)=>
-{
-    const reg = getKeyReg(key);
-    if (!KeepRawValue) {
-       value = encodeURIComponent(value);
-    }
-    url = resetUrl(url);
-    url = reg.test(url) ?
-        url.replace(reg,"$1"+value) :
-        url+ ( (-1 === url.indexOf('?')) ? '?' : '&' )+ key+ '='+ value;
-    return url;
-}
+const getUrl = (key, url) => {
+  url = resetUrl(url);
+  const reg = getKeyReg(key);
+  const exec = reg.exec(url);
+  return !exec ? '' : decodeURIComponent(exec[3]);
+};
+
+const unsetUrl = (key, url) => {
+  const reg = getKeyReg(key);
+  url = resetUrl(url);
+  const exec = reg.exec(url);
+  if (exec) {
+    url = exec[2] === '?' ? url.replace(reg, '?') : url.replace(reg, '');
+  }
+  return url;
+};
+
+const setUrl = (key, value, url, KeepRawValue) => {
+  const reg = getKeyReg(key);
+  if (!KeepRawValue) {
+    value = encodeURIComponent(value);
+  }
+  url = resetUrl(url);
+  url = reg.test(url)
+    ? url.replace(reg, '$1' + value)
+    : url + (-1 === url.indexOf('?') ? '?' : '&') + key + '=' + value;
+  return url;
+};
 
 export {getUrl, unsetUrl};
 export default setUrl;
