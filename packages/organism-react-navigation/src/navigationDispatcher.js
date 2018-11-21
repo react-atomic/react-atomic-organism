5fa1a34afe3725dc9d9b95efaec65d72d5b56c5f
Hill <hill@kimo.com>
2018-11-22 03:02:00 +0800

    fixed with new get version

--- packages/organism-react-navigation/src/navigationDispatcher.js
+++ packages/organism-react-navigation/src/navigationDispatcher.js
@@ -1,8 +1,6 @@
-'use strict';
-
 import {Dispatcher} from 'reshow-flux';
 
 const instance = new Dispatcher();
 export default instance;
 
-export const navigationDispatch = instance.dispatch.bind(instance);
+export const navigationDispatch = instance.dispatch;
--- packages/organism-react-navigation/src/stores/navigationStore.js
+++ packages/organism-react-navigation/src/stores/navigationStore.js
@@ -1,24 +1,12 @@
-'use strict';
-
-import {Map} from 'immutable';
 import {ReduceStore} from 'reshow-flux';
 import dispatcher from '../navigationDispatcher';
 import get from 'get-object-value';
 
-class NavigatioStore extends ReduceStore
-{
-
-  getInitialState()
-  {
-      return Map();
-  }
-
-  reduce (state, action)
-  {
-      const id = get(action,['id'],'default');
-      const settings = get(state.get(id), null, ()=>Map()).
-        merge(action.params)
-      return state.set(id, settings);
+class NavigatioStore extends ReduceStore {
+  reduce(state, action) {
+    const id = get(action, ['id'], 'default');
+    const settings = {...get(state.get(id), null, {}), ...action.params};
+    return state.set(id, settings);
   }
 }
 
