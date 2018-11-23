4829f13ada5a22e79bdb32804e12247e424723e0
Hill <hill@kimo.com>
2018-11-23 14:27:14 +0800

    Add toNum and toInt

--- packages/lib/topercent/src/index.js
+++ packages/lib/topercent/src/index.js
@@ -1,12 +1,20 @@
-'use strict';
+const numTypes = '|number|string|';
 
-const toPercent = (num) => percent(num)+'%';
+const toPercent = num => percent(num) + '%';
 
-const percent = (num) => round(num * 100);
+const percent = num => round(num * 100);
 
-const round = (f, precision=2) =>
-    Number(f).toFixed(precision);
+const round = (f, precision = 2) => toNum(f).toFixed(precision);
 
-export {round, percent};
+const toNum = num =>
+  -1 === numTypes.indexOf('|' + typeof num + '|') || !num
+    ? Number(num)
+    : parseFloat(num) === parseInt(num, 10) 
+      ? parseInt(num, 10)
+      : parseFloat(num);
+
+const toInt = num => round(num, 0);
+
+export {round, percent, toNum, toInt};
 
 export default toPercent;
--- packages/lib/topercent/tests/test.js
+++ packages/lib/topercent/tests/test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import toPercent, {percent, round} from '../src/index.js';
+import toPercent, {percent, round, toNum} from '../cjs/src/index.js';
 import {expect} from 'chai';
 
 describe('Test percent', ()=>{
@@ -16,3 +16,36 @@ describe('Test percent', ()=>{
     });
 });
 
+describe('to num', ()=>{
+  it('Test bool', ()=>{
+    expect(toNum(true)).to.equal(1);
+    expect(toNum(false)).to.equal(0);
+  });
+  it('Test empty', ()=>{
+    expect(toNum([])).to.equal(0);
+    expect(toNum(null)).to.equal(0);
+    expect(toNum('')).to.equal(0);
+  });
+  it('Test int', ()=>{
+    expect(toNum(1)).to.equal(1);
+  });
+  it('Test float', ()=>{
+    expect(toNum(1.1)).to.equal(1.1);
+  });
+  it('Test int string', ()=>{
+    expect(toNum('1')).to.equal(1);
+  });
+  it('Test float string', ()=>{
+    expect(toNum('1.1')).to.equal(1.1);
+  });
+  it('Test Hexadecimal', ()=>{
+    expect(toNum(0xa)).to.equal(10);
+  });
+  it('Test 4.89e7', ()=>{
+    expect(toNum(4.89e7)).to.equal(48900000);
+    expect(toNum('4.89e7')).to.equal(48900000);
+  });
+  it('Test 100.01f', ()=>{
+    expect(toNum('100.01f')).to.equal(100.01);
+  });
+});
