b6aa42540c620bb54e67c2fd9daf688b764f46df
Hill <hill@kimo.com>
2018-11-27 18:57:08 +0800

    improve toNum

--- packages/lib/topercent/src/index.js
+++ packages/lib/topercent/src/index.js
@@ -8,12 +8,19 @@ const round = (f, precision = 2) => toNum(f).toFixed(precision);
 
 const toRoundNum = (f, precision = 2) => toNum(round(f, precision));
 
-const toNum = num =>
-  -1 === numTypes.indexOf('|' + typeof num + '|') || !num
-    ? Number(num)
-    : parseFloat(num) === parseInt(num, 10) 
-      ? parseInt(num, 10)
-      : parseFloat(num);
+const toNum = num => {
+  if (-1 === numTypes.indexOf('|' + typeof num + '|') || !num) {
+    return Number(num);
+  } else {
+    const maybeFloat = parseFloat(num);
+    const maybeInt = parseInt(num, 10);
+    if (maybeFloat === maybeInt) {
+      return maybeInt;
+    } else {
+      return isNaN(maybeFloat) ? 0 : maybeFloat;
+    }
+  }
+}
 
 const toInt = num => toNum(round(num, 0));
 
