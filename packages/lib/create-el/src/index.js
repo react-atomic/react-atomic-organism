b0370cc3cd7e06e9ef04797ae35e2904f2bbc901
Hill <hill@kimo.com>
2018-12-27 12:12:06 +0000

    fixed readyState not exists

--- packages/lib/create-el/src/index.js
+++ packages/lib/create-el/src/index.js
@@ -1,94 +1,86 @@
-
 const doc = () => document;
 
 const keys = Object.keys;
 
 const inject = (base, isStart) => dNode => {
-    if ('function' === typeof base) {
-        base = base();
-    }
-    if (base && (base.nodeName === 'BODY' || base.nodeName === 'HEAD')) {
-        if (isStart && base.firstChild) {
-            base.insertBefore(dNode, base.firstChild);
-            return;
-        } else {
-            base.appendChild(dNode);
-            return;
-        }
+  if ('function' === typeof base) {
+    base = base();
+  }
+  if (base && (base.nodeName === 'BODY' || base.nodeName === 'HEAD')) {
+    if (isStart && base.firstChild) {
+      base.insertBefore(dNode, base.firstChild);
+      return;
     } else {
-        const d = doc();
-        if (!base) {
-            base = d.currentScript ? d.currentScript : d.body;
-        }
-        const parentNode = base.parentNode;
-        if (parentNode) {
-            if (!isStart) {
-                if (base.nextSibling) {
-                    parentNode.insertBefore(dNode, base.nextSibling);
-                    return;
-                } else {
-                    parentNode.appendChild(dNode);
-                    return;
-                }
-            } else {
-                parentNode.insertBefore(dNode, base);
-                return;
-            }
-        }
-        d.body.appendChild(dNode);
+      base.appendChild(dNode);
+      return;
     }
-}
-
-const create = tag => callback => attrs =>
-{
+  } else {
     const d = doc();
-    const dNode = d.createElement(tag);
-    if (attrs) {
-      keys(attrs).forEach(
-          key => dNode[key] = attrs[key]
-      );
+    if (!base) {
+      base = d.currentScript ? d.currentScript : d.body;
     }
-    if (callback) {
-        dNode.onreadystatechange =
-        dNode.onload = () => {
-          if (!this.readyState ||
-              -1 !== '|loaded|complete|'.indexOf('|'+this.readyState+'|')
-          ) {
-            setTimeout(callback);
-          }
+    const parentNode = base.parentNode;
+    if (parentNode) {
+      if (!isStart) {
+        if (base.nextSibling) {
+          parentNode.insertBefore(dNode, base.nextSibling);
+          return;
+        } else {
+          parentNode.appendChild(dNode);
+          return;
         }
+      } else {
+        parentNode.insertBefore(dNode, base);
+        return;
+      }
     }
-    return dNode;
-}
-
-const js = (base, isStart) => callback => (url, attrs) =>
-{
-    const dNode = create('script')(callback)({
-        type: 'text/javascript',
-        ...attrs
-    })
-    if (base) {
-        inject(base, isStart)(dNode);
-    }
-    dNode.src = url;
-    return dNode;
-}
-
-const css = (base, isStart) => callback => (url, attrs) =>
-{
-    const dNode = create('link')(callback)({
-        rel: 'stylesheet',
-        type: 'text/css',
-        ...attrs
-    })
-    if (base) {
-        inject(base, isStart)(dNode);
-    }
-    dNode.href = url;
-    return dNode;
-}
-
-export {
-    js, css, inject, create
+    d.body.appendChild(dNode);
+  }
 };
 
+const create = tag => callback => attrs => {
+  const d = doc();
+  const dNode = d.createElement(tag);
+  if (attrs) {
+    keys(attrs).forEach(key => (dNode[key] = attrs[key]));
+  }
+  if (callback) {
+    dNode.onreadystatechange = dNode.onload = () => {
+      const readyState = dNode.readyState;
+      if (
+        !readyState ||
+        -1 !== '|loaded|complete|'.indexOf('|' + readyState + '|')
+      ) {
+        setTimeout(callback);
+      }
+    };
+  }
+  return dNode;
+};
+
+const js = (base, isStart) => callback => (url, attrs) => {
+  const dNode = create('script')(callback)({
+    type: 'text/javascript',
+    ...attrs,
+  });
+  if (base) {
+    inject(base, isStart)(dNode);
+  }
+  dNode.src = url;
+  return dNode;
+};
+
+const css = (base, isStart) => callback => (url, attrs) => {
+  const dNode = create('link')(callback)({
+    rel: 'stylesheet',
+    type: 'text/css',
+    ...attrs,
+  });
+  if (base) {
+    inject(base, isStart)(dNode);
+  }
+  dNode.href = url;
+  return dNode;
+};
+
+export {js, css, inject, create};
