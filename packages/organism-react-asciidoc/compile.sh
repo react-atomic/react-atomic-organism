417cb2cf69097ebf4c362fcdbc5c9538265fbb50
Hill <hill@kimo.com>
2018-12-28 04:16:51 +0000

    update asciidoc version

--- packages/organism-react-asciidoc/compile.sh
+++ packages/organism-react-asciidoc/compile.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
-#find ./assets -name "*.js" | xargs rm -rf
 
-production(){
-    echo "Production Mode";
-    NODE_ENV=production webpack -p 
-}
-
-develop(){
-    echo "Develop Mode";
-    npm run build
-}
-
-case "$1" in
-  p)
-    production
-    ;;
-  *)
-    develop
-    exit
-esac
+npm run build
 
 exit $?
--- packages/organism-react-asciidoc/ui/organisms/Asciidoc.jsx
+++ packages/organism-react-asciidoc/ui/organisms/Asciidoc.jsx
@@ -3,50 +3,58 @@ import get from 'get-object-value';
 import Iframe from 'organism-react-iframe';
 import {Unsafe} from 'react-atomic-molecule';
 
-const VERSION = '[VERSION]'
+const VERSION = '[VERSION]';
 
-class Asciidoc extends PureComponent 
-{
-   render()
-   {
+class Asciidoc extends PureComponent {
+  render() {
     const {js, css, npmVersion, children, attributes} = this.props;
     let {options} = this.props;
     options = get(options, null, {});
     options.attributes = {
-        ...get(options, ['attributes'], {}),
-        ...attributes
-    }
+      ...get(options, ['attributes'], {}),
+      ...attributes,
+    };
 
-    const thisJs = js.replace(VERSION, npmVersion)
-    const thisCss = css.replace(VERSION, npmVersion)
+    const thisJs = js.replace(VERSION, npmVersion);
+    const thisCss = css.replace(VERSION, npmVersion);
 
     const html = [
-        `<link rel="stylesheet" type="text/css" href="${thisCss}" />`,
-        `<script src="${thisJs}"></script>`,
-	'<div id="data" style="display:none;">'+children+'</div>',
-	'<div id="output"></div>',
-        `
+      `<link rel="stylesheet" type="text/css" href="${thisCss}" />`,
+      `<script src="${thisJs}"></script>`,
+      '<div id="data" style="display:none;">' + children + '</div>',
+      '<div id="output"></div>',
+      `
         <script>
 	    var asciidoctor = Asciidoctor();
-	    var html = asciidoctor.convert(document.getElementById('data').innerHTML, ${JSON.stringify(options)}) 
-	    document.getElementById('output').innerHTML = html
+	    var html = asciidoctor.convert(
+              document.getElementById('data').innerHTML,
+              ${JSON.stringify(options,)}
+            );
+	    document.getElementById('output').innerHTML = html;
 	    setTimeout(function(){
 		parent.window.postMessage({
 		    type: 'iframeH|asciidoc',
 		    h: document.body.offsetHeight,
 		}, '*')
-	    },100)
+	    },100);
         </script>
-        ` 
+        `,
     ].join('');
-    return <Iframe scrolling="no"><Unsafe>{html}</Unsafe></Iframe>
-   }
+
+    return (
+      <Iframe scrolling="no">
+        <Unsafe>{html}</Unsafe>
+      </Iframe>
+    );
+  }
 }
 
 Asciidoc.defaultProps = {
-    js: `//cdn.jsdelivr.net/npm/asciidoctor.js@${VERSION}/dist/browser/asciidoctor.min.js`,
-    css: `//cdn.jsdelivr.net/npm/asciidoctor.js@${VERSION}/dist/css/asciidoctor.css`,
-    npmVersion: '1.5.7-beta.1'
-}
+  js:
+    '//cdn.jsdelivr.net/npm/asciidoctor.js@[VERSION]/dist/browser/asciidoctor.min.js',
+  css:
+    '//cdn.jsdelivr.net/npm/asciidoctor.js@[VERSION]/dist/css/asciidoctor.css',
+  npmVersion: '1.5.9',
+};
 
-export default Asciidoc
+export default Asciidoc;
