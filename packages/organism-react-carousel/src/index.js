2435bd4b7caf1e063d79b82d0ef36a0b336f3801
Hill <hill@kimo.com>
2019-01-15 03:52:33 +0000

    improve es6 code style

--- packages/organism-react-carousel/src/index.js
+++ packages/organism-react-carousel/src/index.js
@@ -1,5 +1,5 @@
-export Carousel from '../ui/organisms/Carousel';
-export CarouselList from '../ui/organisms/CarouselList';
-export CarouselAnimation from '../ui/organisms/CarouselAnimation';
-export CarouselSwipe from '../ui/organisms/CarouselSwipe';
-export CarouselSlide from '../ui/organisms/CarouselSlide';
+export { default as Carousel } from '../ui/organisms/Carousel';
+export { default as CarouselList } from '../ui/organisms/CarouselList';
+export { default as CarouselAnimation } from '../ui/organisms/CarouselAnimation';
+export { default as CarouselSwipe } from '../ui/organisms/CarouselSwipe';
+export { default as CarouselSlide } from '../ui/organisms/CarouselSlide';
