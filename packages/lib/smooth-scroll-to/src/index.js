62eb865465c52e336d3bf2e8f7cf314d6c857dfc
Hill <hill@kimo.com>
2019-01-26 23:33:27 +0800

    update index

--- packages/lib/smooth-scroll-to/src/index.js
+++ packages/lib/smooth-scroll-to/src/index.js
@@ -1,59 +1,48 @@
-'use strict';
-
-import { getScrollNode } from 'get-scroll-info';
+import {getScrollNode} from 'get-scroll-info';
 import easeInOutCubic from 'easing-lib/easeInOutCubic';
 
 let isRunning = false;
+
+const call = func => ('function' === typeof func ? func : () => {});
 
 /**
  *  !!Important!! any logic change need take care isRunning
  */
 const smoothScrollTo = (to, duration, el, callback) => {
-    el = getScrollNode(el)
-    if (isRunning) {
-        isRunning = false
-        setTimeout(() => {
-            el.scrollTop = to
-            if ('function' === typeof callback) {
-                callback()
-            }
-        })
-        return false;
+  const scrollNode = getScrollNode(el);
+  if (isRunning) {
+    isRunning = false;
+    setTimeout(() => {
+      scrollNode.scrollTop = to;
+      call(callback)(scrollNode);
+    });
+    return false;
+  }
+  if (!duration) {
+    duration = 900;
+  }
+  const from = scrollNode.scrollTop;
+  const go = to - from;
+  if (!go) {
+    isRunning = false;
+    call(callback)(scrollNode);
+    return;
+  }
+  let beginTimeStamp;
+  const scrollTo = timeStamp => {
+    beginTimeStamp = beginTimeStamp || timeStamp;
+    const elapsedTime = timeStamp - beginTimeStamp;
+    const progress = easeInOutCubic(elapsedTime, from, go, duration);
+    scrollNode.scrollTop = progress;
+    if (elapsedTime < duration && go && isRunning) {
+      requestAnimationFrame(scrollTo);
+    } else {
+      isRunning = false;
+      call(callback)(scrollNode);
     }
-    if (!duration) {
-        duration = 900;
-    }
-    const from = el.scrollTop;
-    const go = to - from;
-    if (!go) {
-        isRunning = false;
-        if ('function' === typeof callback) {
-            callback();
-        }
-        return;
-    }
-    let beginTimeStamp;
-    const scrollTo = (timeStamp) => {
-        beginTimeStamp = beginTimeStamp || timeStamp;
-        const elapsedTime = timeStamp - beginTimeStamp;
-        const progress = easeInOutCubic(
-            elapsedTime,
-            from,
-            go,
-            duration
-        );
-        el.scrollTop = progress;
-        if ( elapsedTime < duration && go && isRunning) { 
-            requestAnimationFrame(scrollTo);
-        } else {
-            isRunning = false;
-            if ('function' === typeof callback) {
-                callback();
-            }
-        }
-    }
-    isRunning = true
-    requestAnimationFrame(scrollTo);
+  };
+  isRunning = true;
+  requestAnimationFrame(scrollTo);
 };
 
 export default smoothScrollTo;
