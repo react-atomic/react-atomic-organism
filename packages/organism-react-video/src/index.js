a7b68b7cef8bd261b8d1869ce8ab51a0f34e60b8
Hill <hill@kimo.com>
2019-02-20 04:01:32 +0000

    Add VideoThumbnail

--- packages/organism-react-video/src/index.js
+++ packages/organism-react-video/src/index.js
@@ -1,4 +1,5 @@
 export { default as ResponsiveVideo } from '../ui/organisms/ResponsiveVideo';
 export { default as Video }  from '../ui/organisms/Video';
 export { default as VideoFile }  from '../ui/organisms/VideoFile';
+export { default as VideoThumbnail }  from '../ui/organisms/VideoThumbnail';
 export { default as YoutubeRWD } from '../ui/organisms/YoutubeRWD';
