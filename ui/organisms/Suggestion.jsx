f9b6ec332fb7954a0348b7c909c1b2a5ff1714ba
Hill <hill@kimo.com>
2019-03-19 08:58:21 +0000

    fixed Suggestion event not found

--- ui/organisms/Suggestion.jsx
+++ ui/organisms/Suggestion.jsx
@@ -1,131 +1,127 @@
-import React, {PureComponent, cloneElement, createElement, isValidElement} from 'react'
-import get from 'get-object-value'
-import {doc} from 'win-doc'
+import React, {
+  PureComponent,
+  cloneElement,
+  createElement,
+  isValidElement,
+} from 'react';
+import get from 'get-object-value';
+import {doc} from 'win-doc';
+import callfunc from 'call-func';
 
-import SearchBox from '../organisms/SearchBox'
+import SearchBox from '../organisms/SearchBox';
 
-const body = () => doc().body
+const body = () => doc().body;
 
-class Suggestion extends PureComponent
-{
+class Suggestion extends PureComponent {
   static defaultProps = {
     itemClickToClose: true,
-    component: SearchBox
-  }
+    component: SearchBox,
+  };
 
   state = {
-    value: ''
-  }
+    value: '',
+  };
 
-  open(e)
-  {
+  open(e) {
     if (this.state.isOpen) {
-      return
+      return;
     }
-    body().addEventListener(
-      'click',
-      this.handleClose
-    )
-    this.setValue(undefined, e)
+    body().addEventListener('click', this.handleClose);
+    this.setValue(undefined, e);
   }
 
-  close()
-  {
-    body().removeEventListener(
-      'click',
-      this.handleClose
-    )
-    this.setState({isOpen: false})
+  close() {
+    body().removeEventListener('click', this.handleClose);
+    this.setState({isOpen: false});
   }
 
-  setValue(value, e)
-  {
-    const input = this.input
-    let nextState = {value}
+  setValue(value, e) {
+    const input = this.input;
+    let nextState = {value};
     if ('undefined' === typeof value) {
       nextState = {
         value: input.value,
-        isOpen: true
-      }
+        isOpen: true,
+      };
     }
-    e.persist()
-    this.setState(nextState, ()=>{
-      const {onChange} = this.props
-      if ('function' === typeof onChange) {
-        onChange(e, this.state.value, input.name)
-      }
-    })
+    if (e) {
+      e.persist();
+    }
+    this.setState(nextState, () => {
+      const {onChange} = this.props;
+      callfunc(onChange, [e, this.state.value, input.name]);
+    });
   }
 
   handleClose = e => {
-    const target = e.target
-    const sbox = this.searchbox
+    const target = e.target;
+    const sbox = this.searchbox;
     if (sbox.contains(target)) {
-      return
+      return;
     }
-    this.close()
-  }
+    this.close();
+  };
 
   handleInput = e => {
-    const value = get(e, ['target', 'value'], '') 
-    this.setValue(value, e)
-  }
+    const value = get(e, ['target', 'value'], '');
+    this.setValue(value, e);
+  };
 
   handleFocus = e => {
-    this.open(e)
-  }
+    this.open(e);
+  };
 
-  static getDerivedStateFromProps(nextProps, prevState)
-  {
-    const {value} = nextProps
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const {value} = nextProps;
     if (value !== prevState.prevPropsValue) {
       return {
         value,
-        prevPropsValue: value
-      }
+        prevPropsValue: value,
+      };
     } else {
-      return null
+      return null;
     }
   }
 
-  componentWillUnmount()
-  {
-    this.close()
+  componentWillUnmount() {
+    this.close();
   }
 
-  render()
-  {
-    const {component, wrapRefCb, onChange, onFocus, results, itemOnClick, itemClickToClose, ...props} = this.props
-    const {value: stateValue, isOpen} = this.state
-    let thisResults = null
+  render() {
+    const {
+      component,
+      wrapRefCb,
+      onChange,
+      onFocus,
+      results,
+      itemOnClick,
+      itemClickToClose,
+      ...props
+    } = this.props;
+    const {value: stateValue, isOpen} = this.state;
+    let thisResults = null;
     if (isOpen) {
-      thisResults = results
+      thisResults = results;
     }
     if ('function' === typeof itemOnClick) {
-      props.itemOnClick = (e, item) =>
-      {
-        itemOnClick(e, item)
+      props.itemOnClick = (e, item) => {
+        itemOnClick(e, item);
         if (itemClickToClose) {
-          this.close()
+          this.close();
         }
-      }
+      };
     }
-    const build = (isValidElement(component)) ?
-              cloneElement :
-              createElement
-    return build(
-      component,
-      {
-        ...props,
-        wrapRefCb: el => this.searchbox = el,
-        refCb: el => this.input = el,
-        value: stateValue,
-        onChange: this.handleInput,
-        onFocus: this.handleFocus,
-        results: thisResults
-      }
-    )
+    const build = isValidElement(component) ? cloneElement : createElement;
+    return build(component, {
+      ...props,
+      wrapRefCb: el => (this.searchbox = el),
+      refCb: el => (this.input = el),
+      value: stateValue,
+      onChange: this.handleInput,
+      onFocus: this.handleFocus,
+      results: thisResults,
+    });
   }
 }
 
-export default Suggestion
+export default Suggestion;
