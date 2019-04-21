b7fecc21c2ca24b2e60ab8995e1d3055c8dfb989
Hill <hill@kimo.com>
2019-04-21 18:47:47 +0800

    Add default width: 100% of svg

--- packages/organism-react-graph/ui/molecules/BaseString.jsx
+++ packages/organism-react-graph/ui/molecules/BaseString.jsx
@@ -1,56 +1,44 @@
-import React, {PureComponent} from 'react'
-import get from 'get-object-value'
+import React, {PureComponent} from 'react';
+import get from 'get-object-value';
 
-class BaseString extends PureComponent
-{
-    state = {}
+class BaseString extends PureComponent {
+  state = {};
 
-    resetProps(thisProps, thisState)
-    {
-       const {
-        parentWidth,
-        alignCenter,
-        x,
-        y,
-        ...props
-       } = thisProps
-       let {x: stateX, y: stateY} = thisState
-       let thisX = x
-       let thisY = y
-       if (alignCenter) {
-            thisX = stateX
-       }
-       if (thisX) {
-            props['x'] = thisX
-       }
-       if (thisY) {
-            props['y'] = thisY
-       }
-       return props
+  resetProps(thisProps, thisState) {
+    const {parentWidth, alignCenter, x, y, ...props} = thisProps;
+    let {x: stateX, y: stateY} = thisState;
+    let thisX = x;
+    let thisY = y;
+    if (alignCenter) {
+      thisX = stateX;
     }
-
-    update(props, prevProps)
-    {
-        prevProps = get(prevProps, null, {})
-        const el = this.el
-        const {parentWidth, alignCenter} = props  
-        if (alignCenter && parentWidth !== prevProps.parentWidth ) {
-            const {width} = el.getBBox()
-            const x = (parentWidth - width) / 2 
-            this.setState({x})
-        }
+    if (thisX) {
+      props.x = thisX;
     }
-
-    componentDidUpdate(prevProps, prevState, snapshot)
-    {
-        this.update(this.props, prevProps)
+    if (thisY) {
+      props.y = thisY;
     }
+    return props;
+  }
 
-    componentDidMount()
-    {
-        this.update(this.props)
+  update(props, prevProps) {
+    prevProps = get(prevProps, null, {});
+    const el = this.el;
+    const {parentWidth, alignCenter} = props;
+    if (alignCenter && parentWidth !== prevProps.parentWidth) {
+      const {width} = el.getBBox();
+      const x = (parentWidth - width) / 2;
+      this.setState({x});
     }
+  }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    this.update(this.props, prevProps);
+  }
+
+  componentDidMount() {
+    this.update(this.props);
+  }
 }
 
-export default BaseString
+export default BaseString;
--- packages/organism-react-graph/ui/organisms/Svg.jsx
+++ packages/organism-react-graph/ui/organisms/Svg.jsx
@@ -3,6 +3,9 @@ import {SemanticUI} from 'react-atomic-molecule';
 
 const Svg = props => <SemanticUI {...props} />;
 
-Svg.defaultProps = {atom: 'svg'};
+Svg.defaultProps = {
+  atom: 'svg',
+  width: '100%'
+};
 
 export default Svg;
