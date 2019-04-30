3d2cd9a91784c813c7e02a0b4c62beebd5088634
Hill <hill@kimo.com>
2019-04-30 17:43:31 +0800

    improve curve support

--- packages/organism-react-graph/ui/organisms/Area.jsx
+++ packages/organism-react-graph/ui/organisms/Area.jsx
@@ -1,38 +1,19 @@
-import React, {PureComponent} from 'react'
-import {SemanticUI} from 'react-atomic-molecule'
-import {hArea} from 'd3-lib'
+import React, {PureComponent} from 'react';
+import {SemanticUI} from 'react-atomic-molecule';
+import {hArea} from 'd3-lib';
 
-class Area extends PureComponent
-{
-    static defaultProps = {
-        atom: 'path',
-        fillOpacity: '0.1',
-        stroke: 'none'
-    }
+class Area extends PureComponent {
+  static defaultProps = {
+    atom: 'path',
+    fillOpacity: '0.1',
+    stroke: 'none',
+  };
 
-    render()
-    {
-       const {
-        data,
-        xLocator,
-        y0Locator,
-        y1Locator,
-        ...props
-       } = this.props
-       const d = hArea(
-        data,
-        xLocator,
-        y0Locator,
-        y1Locator
-       )
-       return (
-            <SemanticUI 
-                ui={false}
-                {...props}
-                d={d}
-            />
-       )
-    }
+  render() {
+    const {curve, data, xLocator, y0Locator, y1Locator, ...props} = this.props;
+    const d = hArea(data, xLocator, y0Locator, y1Locator, curve);
+    return <SemanticUI ui={false} {...props} d={d} />;
+  }
 }
 
-export default Area
+export default Area;
--- packages/organism-react-graph/ui/organisms/Line.jsx
+++ packages/organism-react-graph/ui/organisms/Line.jsx
@@ -1,32 +1,24 @@
-import React, {PureComponent} from 'react'
-import {SemanticUI} from 'react-atomic-molecule'
+import React, {PureComponent} from 'react';
+import {SemanticUI} from 'react-atomic-molecule';
 import {line} from 'd3-lib';
 
-class Line extends PureComponent
-{
-    render()
-    {
-       const {start, end, svgLine, curve, ...props} = this.props
-        const params = {};
-        if (start && end) {
-           if (svgLine) {
-             params.x1 = start.x;
-             params.y1 = start.y;
-             params.x2 = end.x;
-             params.y2 = end.y;
-           } else {
-             params.d = line(start, end, curve)
-           }
-        }
-        params.atom = (svgLine) ? 'line': 'path'
-       return (
-            <SemanticUI 
-                ui={false}
-                {...props}
-                {...params}
-            />
-       )
+class Line extends PureComponent {
+  render() {
+    const {start, end, svgLine, curve, ...props} = this.props;
+    const params = {};
+    if (start && end) {
+      if (svgLine) {
+        params.x1 = start.x;
+        params.y1 = start.y;
+        params.x2 = end.x;
+        params.y2 = end.y;
+      } else {
+        params.d = line(start, end, curve);
+      }
     }
+    params.atom = svgLine ? 'line' : 'path';
+    return <SemanticUI ui={false} {...props} {...params} />;
+  }
 }
 
-export default Line 
+export default Line;
