3784c9f618da03ae8ad93f693d0c362f1c72ea8f
Hill <hill@kimo.com>
2019-05-14 02:24:38 +0800

    Add Polygon

---
+++ packages/organism-react-graph/ui/organisms/Polygon.jsx
@@ -0,0 +1,11 @@
+import React, {PureComponent} from 'react';
+import {SemanticUI} from 'react-atomic-molecule';
+
+class Polygon extends PureComponent {
+  render() {
+    const props = this.props;
+    return <SemanticUI atom="polygon" ui={false} {...props} />;
+  }
+}
+
+export default Polygon;
