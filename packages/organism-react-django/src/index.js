e984fde9734cff33322deafc5a93325e1860b0ca
Hill <hill@kimo.com>
2019-08-07 01:10:00 +0800

    update index.js

--- packages/organism-react-django/src/index.js
+++ packages/organism-react-django/src/index.js
@@ -1,1 +1,3 @@
-export {default as TagsField} from '../ui/organisms/TagsField';
+export {default as DjangoForm} from '../ui/organisms/DjangoForm';
+
+export {default as djangoFormParser} from './djangoFormParser';
