8d6bd1a6e97d7c7a232f832619397c495445e79f
Hill <hill@kimo.com>
2019-11-10 22:14:58 +0800

    improve handle error

--- packages/organism-react-popup/ui/molecules/BasePopup.jsx
+++ packages/organism-react-popup/ui/molecules/BasePopup.jsx
@@ -1,10 +1,21 @@
-import React, {PureComponent} from 'react'; 
+import React, {PureComponent} from 'react';
 
-class BasePopup extends PureComponent
-{
-    static defaultProps = {
-        name: 'default'
+class BasePopup extends PureComponent {
+  static defaultProps = {
+    name: 'default',
+  };
+  static getDerivedStateFromError(error) {
+    return {show: false};
+  }
+  componentDidCatch(error, info) {
+    const {onError} = this.props;
+    if (onError) {
+      callfunc(onError, [error, info]);
+    } else {
+      console.error([error, info]);
     }
+    this.setState({show: false});
+  }
 }
 
 export default BasePopup;
