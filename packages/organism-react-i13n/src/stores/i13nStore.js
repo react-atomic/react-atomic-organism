7a8d9bc19eb54692e711786385a39aa0af6d43a7
Hill <hill@kimo.com>
2019-12-15 05:15:46 +0000

    fixed view not call callback

--- packages/organism-react-i13n/src/stores/i13nStore.js
+++ packages/organism-react-i13n/src/stores/i13nStore.js
@@ -6,54 +6,45 @@ import {ajaxDispatch} from 'organism-react-ajax';
 
 import {i13nDispatcher, BaseI13nStore} from 'i13n';
 
-const getDefaultActionCallback = (state) => 
-    (json, text)=>{
-        const element = state.get('element');
-        const iframe = get(element,['iframe']);
-        if (iframe) {
-            iframe.appendHtml(text);
-        } 
-    }
+const getDefaultActionCallback = state => (json, text) => {
+  const element = state.get('element');
+  const iframe = get(element, ['iframe']);
+  if (iframe) {
+    iframe.appendHtml(text);
+  }
+};
 
-class I13nStore extends BaseI13nStore
-{
-
-  getInitialState()
-  {
-      return Map();
+class I13nStore extends BaseI13nStore {
+  getInitialState() {
+    return Map();
   }
 
-  sendBeacon(state, action)
-  {
-        const pvid = state.get('pvid');
-        const src = state.get('src');
-        const params = action.params;
-        const query = get(params, ['query'], []);
-        const callback = get(
-            params,
-            ['callback'],
-            () => {
-                // default cb for action
-                return getDefaultActionCallback(state);
-            }
-        );
-        setImmediate(()=>{
-            ajaxDispatch({
-                type: 'ajaxPost',
-                params: {
-                   url: src+action.type,
-                   query: {
-                       pvid: pvid,
-                       url: document.URL,
-                       params: get(params, ['I13N']),
-                       ...query
-                   },
-                   callback: callback,
-                   disableProgress: true 
-                }
-            });
-        });
-        return state;
+  sendBeacon(state, action) {
+    const pvid = state.get('pvid');
+    const src = state.get('src');
+    const params = action.params;
+    const query = get(params, ['query'], []);
+    const callback = get(params, ['callback'], () => {
+      // default cb for action
+      return getDefaultActionCallback(state);
+    });
+    setImmediate(() => {
+      ajaxDispatch({
+        type: 'ajaxPost',
+        params: {
+          url: src + action.type,
+          query: {
+            pvid: pvid,
+            url: document.URL,
+            params: get(params, ['I13N']),
+            ...query,
+          },
+          callback,
+          disableProgress: true,
+        },
+      });
+    });
+    return state;
   }
 }
 
