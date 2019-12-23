ee01ab0472e43d314d498ceddad82824f6637618
Hill <hill@kimo.com>
2019-12-23 10:44:58 +0800

    Add handle click link

--- packages/organism-react-codeeditor/ui/organisms/Preview.jsx
+++ packages/organism-react-codeeditor/ui/organisms/Preview.jsx
@@ -3,6 +3,7 @@ import Iframe from 'organism-react-iframe';
 import {win} from 'win-doc';
 import {SemanticUI, Unsafe, Icon} from 'react-atomic-molecule';
 import Device from 'ricon/Device';
+import {popupDispatch, AlertsNotifier} from 'organism-react-popup';
 
 const deviceWidth = {
   desktop: 1024,
@@ -13,7 +14,7 @@ const deviceWidth = {
 const keys = Object.keys;
 
 class Preview extends PureComponent {
-  state = {width: deviceWidth.desktop, scale: 1};
+  state = {width: deviceWidth.desktop, scale: 1, height: '100%'};
 
   setValue(v) {
     this.setState({v});
@@ -21,26 +22,48 @@ class Preview extends PureComponent {
 
   resize = () => {
     const {width} = this.state;
-    const iframeW = this.dIframe.offsetWidth;
-    let scale = (iframeW - 10) / width;
+    const containerW = this.container.offsetWidth;
+    let scale = (containerW - 10) / width;
+    let height = 100 / scale;
     if (scale > 1) {
       scale = 1;
+      height = 100;
     }
-    this.setState({scale});
+    this.setState({scale, height});
   };
 
   changeDevice = device => () => {
-    this.setState({width: deviceWidth[device]}, ()=>this.resize());
+    this.setState({width: deviceWidth[device]}, () => this.resize());
   };
 
-  handleIframe = el => {
-    this.dIframe = el;
-  };
+  handleIframe = el => (this.dIframe = el);
+  handleContainer = el => (this.container = el);
 
   handleLoad = () => {
     this.iframeWindow = this.dIframe.contentWindow.window;
-    this.resize();
-    win().addEventListener('resize', this.resize);
+    let timer;
+    timer = setInterval(() => {
+      if (this.container) {
+        this.resize();
+        win().addEventListener('resize', this.resize);
+      }
+    }, 10);
+  };
+
+  handleLinkClick = (e, link) => {
+    const alerts = [
+      {
+        message: (
+          <SemanticUI atom="a" href={link.href} target="_blank">
+            {`click: ${link.href}`}
+          </SemanticUI>
+        ),
+      },
+    ];
+    popupDispatch('dom/update', {
+      popup: <AlertsNotifier alerts={alerts} position="bottom" />,
+    });
+    return false;
   };
 
   componentWillUnmount() {
@@ -48,22 +71,17 @@ class Preview extends PureComponent {
   }
 
   render() {
-    const {v, scale, width} = this.state;
-    let rwdStyle = null;
-    let bodyStyle = null;
+    const {v, scale, width, height} = this.state;
+    let bgStyle = null;
     if (width !== deviceWidth.desktop) {
-      rwdStyle = {
-        background: '#fff',
-        margin: '0 auto',
-        overflow: 'hidden'
-      }; 
-      bodyStyle = 'body {background: rgba(0, 0, 0, .6)}';
+      bgStyle = {background: 'rgba(0, 0, 0, .6)'};
     }
     const {className} = this.props;
     return (
       <SemanticUI
+        refCb={this.handleContainer}
         className={className}
-        style={{...Styles.fitHeight, ...Styles.container}}>
+        style={{...Styles.container, ...bgStyle}}>
         <SemanticUI style={Styles.device}>
           {keys(deviceWidth).map(d => (
             <Icon key={d} style={Styles.icon} onClick={this.changeDevice(d)}>
@@ -72,19 +90,18 @@ class Preview extends PureComponent {
           ))}
         </SemanticUI>
         <Iframe
-          style={{...Styles.fitHeight, ...Styles.iframe}}
+          style={{
+            ...Styles.iframe,
+            transform: `scale(${scale}) translateX(-50%)`,
+            transformOrigin: '0 0',
+            width,
+            height: height + '%',
+          }}
+          onLinkClick={this.handleLinkClick}
           refCb={this.handleIframe}
           onLoad={this.handleLoad}>
-          <Unsafe atom="style">{`body {overflow-x: hidden} ${bodyStyle}`}</Unsafe>
-          <Unsafe
-            style={{
-              ...rwdStyle,
-              transform: `scale(${scale})`,
-              transformOrigin: '0 0',
-              width,
-            }}>
-            {v}
-          </Unsafe>
+          <Unsafe atom="style">{`body {overflow-x: hidden; background: #fff}`}</Unsafe>
+          <Unsafe>{v}</Unsafe>
         </Iframe>
       </SemanticUI>
     );
@@ -98,22 +115,21 @@ const Styles = {
     width: 24,
     height: 24,
     marginLeft: 10,
-  },
-  fitHeight: {
-    height: '100%',
+    cursor: 'pointer',
   },
   container: {
     position: 'relative',
+    height: '100%',
   },
   iframe: {
     position: 'absolute',
-    left: 0,
+    left: '50%',
     top: 0,
     zIndex: 0,
   },
   device: {
     position: 'relative',
     zIndex: 1,
-    marginTop: 10
+    marginTop: 10,
   },
 };
