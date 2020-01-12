e1ba647ad95227fbd8fcd3dc7fab591c92f78340
Hill <hill@kimo.com>
2020-01-12 18:01:02 +0800

    fixed chain error

--- packages/organism-react-spotlight/src/cleanZIndex.js
+++ packages/organism-react-spotlight/src/cleanZIndex.js
@@ -2,31 +2,30 @@ import getStyle from 'get-style';
 import setClass from './setClass';
 
 const cleanZIndex = (
-    node,
-    classCleanZIndex = 'react-spotlight-clean-zindex',
-    classShowEl = ''
-) =>
-{
-    let thisParent = node.parentNode;
-    if (!thisParent) {
-        return;
-    }
-    let isFindFixedParent;
-    while(thisParent.nodeName != 'BODY') {
-        const zIndex = getStyle(thisParent, 'z-index');
-        if (zIndex && 'auto' !== zIndex) {
-            const position =  getStyle(thisParent, 'position');
-            if ('fixed' !== position || !classShowEl) {
-                setClass(thisParent, [classCleanZIndex]);
-            } else if ('fixed' === position) {
-                if (!isFindFixedParent) {
-                    isFindFixedParent = true;
-                    setClass(thisParent, [classShowEl]);
-                }
-            }
+  node,
+  classCleanZIndex = 'react-spotlight-clean-zindex',
+  classShowEl = '',
+) => {
+  let thisParent = node.parentNode;
+  if (!thisParent) {
+    return;
+  }
+  let isFindFixedParent;
+  while (thisParent && thisParent.nodeName != 'BODY') {
+    const zIndex = getStyle(thisParent, 'z-index');
+    if (zIndex && 'auto' !== zIndex) {
+      const position = getStyle(thisParent, 'position');
+      if ('fixed' !== position || !classShowEl) {
+        setClass(thisParent, [classCleanZIndex]);
+      } else if ('fixed' === position) {
+        if (!isFindFixedParent) {
+          isFindFixedParent = true;
+          setClass(thisParent, [classShowEl]);
         }
-        thisParent = thisParent.parentNode;
+      }
     }
-}
+    thisParent = thisParent.parentNode;
+  }
+};
 
 export default cleanZIndex;
