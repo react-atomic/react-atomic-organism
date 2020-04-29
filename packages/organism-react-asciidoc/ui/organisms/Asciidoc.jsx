6b5004568a69c39d8fdc3e88666bafdaea4c5287
Hill <hill@kimo.com>
2020-04-29 14:09:37 +0800

    improve onload

--- packages/organism-react-asciidoc/ui/organisms/Asciidoc.jsx
+++ packages/organism-react-asciidoc/ui/organisms/Asciidoc.jsx
@@ -2,7 +2,7 @@ import React, { PureComponent } from "react";
 import get from "get-object-value";
 import Iframe from "organism-react-iframe";
 import { Unsafe } from "react-atomic-molecule";
-import callfunc from 'call-func';
+import callfunc from "call-func";
 
 const VERSION = "[VERSION]";
 
@@ -11,11 +11,11 @@ class Asciidoc extends PureComponent {
     this.iframe = el;
   };
 
-  handleLoad = () => {
-    const {onLoad} = this.props;
+  handleLoad = el => {
+    const { onLoad } = this.props;
     this.iframe.postHeight();
-    callfunc(onLoad);
-  }
+    callfunc(onLoad, [el]);
+  };
 
   clear() {
     if (this.timer) {
@@ -28,7 +28,7 @@ class Asciidoc extends PureComponent {
     this.timer = setInterval(() => {
       if (oWin.renderDone) {
         this.clear();
-        this.handleLoad();
+        this.handleLoad(oWin.renderDone);
       }
     }, 10);
   }
@@ -38,7 +38,15 @@ class Asciidoc extends PureComponent {
   }
 
   render() {
-    const { js, css, inlineCSS, npmVersion, onLoadDelay, children, attributes } = this.props;
+    const {
+      js,
+      css,
+      inlineCSS,
+      npmVersion,
+      onLoadDelay,
+      children,
+      attributes
+    } = this.props;
     let { options } = this.props;
     options = get(options, null, {});
     options.attributes = {
@@ -61,8 +69,9 @@ class Asciidoc extends PureComponent {
               document.getElementById('data').innerHTML,
               ${JSON.stringify(options)}
             );
-	    document.getElementById('output').innerHTML = html;
-            setTimeout(function(){window.renderDone=true;}, ${onLoadDelay});
+            var output = document.getElementById('output');
+	    output.innerHTML = html;
+            setTimeout(function(){window.renderDone=output;}, ${onLoadDelay});
         </script>
         `
     ].join("");
@@ -77,7 +86,8 @@ class Asciidoc extends PureComponent {
 
 Asciidoc.defaultProps = {
   onLoadDelay: 3500,
-  inlineCSS: 'body {padding: 0; margin: 0; background: transparent !important;}',
+  inlineCSS:
+    "body {padding: 0; margin: 0; background: transparent !important;}",
   js:
     "//cdn.jsdelivr.net/npm/@asciidoctor/core@[VERSION]/dist/browser/asciidoctor.min.js",
   css:
