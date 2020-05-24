9504202ac92c50144fd57845dd1d2855242d0f9a
Hill <hill@kimo.com>
2020-05-24 10:44:27 +0800

    Add getDateObject and getTimestamp

--- packages/lib/get-random-id/src/index.js
+++ packages/lib/get-random-id/src/index.js
@@ -1,1 +1,9 @@
-export default () => new Date().getTime() + '' + Math.random();
+const getDateObject = t => (null != t ? new Date(t) : new Date());
+
+const getTimestamp = t => getDateObject(t).getTime();
+
+const getRandom = () => getTimestamp() + "" + Math.random();
+
+export default getRandom;
+
+export {getDateObject, getTimestamp};
