86649f9ae3293c8ebf8331081559256de3e09ebd
Hill <hill@kimo.com>
2020-05-24 10:55:24 +0800

    Add getTimestampTest

---
+++ packages/lib/get-random-id/src/__tests__/getTimestampTest.js
@@ -0,0 +1,17 @@
+import {expect} from 'chai';
+
+import {getTimestamp} from '../index';
+
+
+describe('Test getTimestamp', () => {
+  const now = new Date().getTime()
+  it('defined time', ()=>{
+    const ut = getTimestamp(now);
+    expect(ut).to.equal(now);
+  });
+
+  it('get new time', ()=>{
+    const ut = getTimestamp();
+    expect(ut > now).to.be.true;
+  });
+});
