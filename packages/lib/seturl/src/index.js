75a2ecd8fe3986d200481cc7796442ad7c13cba9
Hill <hill@kimo.com>
2020-05-26 16:44:22 +0800

    improve path

--- packages/lib/seturl/src/index.js
+++ packages/lib/seturl/src/index.js
@@ -1,9 +1,16 @@
-import {doc} from 'win-doc';
-import getKeyReg from './getKeyReg';
+import { doc } from "win-doc";
+import getKeyReg from "./getKeyReg";
 
 const uriReg = /^(((([^:\/#\?]+:)?(?:(\/\/)((?:(([^:@\/#\?]+)(?:\:([^:@\/#\?]+))?)@)?(([^:\/#\?\]\[]+|\[[^\/\]@#?]+\])(?:\:([0-9]+))?))?)?)?((\/?(?:[^\/\?#]+\/+)*)([^\?#]*)))?(\?[^#]+)?)(#.*)?/;
 
-const getPath = url => uriReg.exec(url)[2]
+const getPath = (url, key, raw) => {
+  const result = uriReg.exec(url);
+  if (raw) {
+    return result;
+  } else {
+    return result[key ?? 2];
+  }
+};
 
 const resetUrl = url => (url ? url : doc().URL);
 
@@ -11,7 +18,7 @@ const getUrl = (key, url) => {
   url = resetUrl(url);
   const reg = getKeyReg(key);
   const exec = reg.exec(url);
-  return !exec ? '' : decodeURIComponent(exec[3]);
+  return !exec ? "" : decodeURIComponent(exec[3]);
 };
 
 const unsetUrl = (key, url) => {
@@ -19,7 +26,7 @@ const unsetUrl = (key, url) => {
   url = resetUrl(url);
   const exec = reg.exec(url);
   if (exec) {
-    url = exec[2] === '?' ? url.replace(reg, '?') : url.replace(reg, '');
+    url = exec[2] === "?" ? url.replace(reg, "?") : url.replace(reg, "");
   }
   return url;
 };
@@ -31,10 +38,10 @@ const setUrl = (key, value, url, KeepRawValue) => {
   }
   url = resetUrl(url);
   url = reg.test(url)
-    ? url.replace(reg, '$1' + value)
-    : url + (-1 === url.indexOf('?') ? '?' : '&') + key + '=' + value;
+    ? url.replace(reg, "$1" + value)
+    : url + (-1 === url.indexOf("?") ? "?" : "&") + key + "=" + value;
   return url;
 };
 
-export {getUrl, getPath, unsetUrl};
+export { getUrl, getPath, unsetUrl };
 export default setUrl;
