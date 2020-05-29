76b459f581286038923b0491ffc09dd173ee95c7
Hill <hill@kimo.com>
2020-05-29 14:47:38 +0800

    add unit test

---
+++ packages/lib/seturl/src/__tests__/test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import set, {getUrl, unsetUrl} from '../index.js';
+import {expect} from 'chai';
+
+global.document = {};
+describe('test get url', ()=>{
+    let document = global.document;
+    it('url', ()=>{
+        document.URL='http://xxx?abc=def';
+        expect(getUrl('abc')).to.equal('def');
+    });
+});
+
+describe('test set url', ()=>{
+    let document = global.document;
+    it('url', ()=>{
+        document.URL='http://xxx?abc=def';
+        expect(set('abc', 'foo')).to.equal('http://xxx?abc=foo');
+    });
+});
+
+describe('test clean url', ()=>{
+    let document = global.document;
+    it('with question mark', ()=>{
+        document.URL='http://xxx?abc=def&foo=bar';
+        expect(unsetUrl('abc')).to.equal('http://xxx?&foo=bar');
+    });
+    it('without question mark', ()=>{
+        document.URL='http://xxx?abc=def&foo=bar';
+        expect(unsetUrl('foo')).to.equal('http://xxx?abc=def');
+    });
+    it('key not exists', ()=>{
+        document.URL='http://xxx?abc=def&foo=bar';
+        expect(unsetUrl('bar')).to.equal('http://xxx?abc=def&foo=bar');
+    });
+});
