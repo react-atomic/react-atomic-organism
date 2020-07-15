02f5a22808e5eb58244a3c1d338a2dc71233e315
Hill <hill@kimo.com>
2020-07-16 04:09:55 +0800

    format alert

--- packages/organism-react-popup/ui/organisms/AlertsNotifier.jsx
+++ packages/organism-react-popup/ui/organisms/AlertsNotifier.jsx
@@ -1,54 +1,54 @@
-import React, {PureComponent, Component} from 'react';
-import PropTypes from 'prop-types';
-import {Set} from 'immutable';
-import Animate from 'organism-react-animate';
-import XIcon from 'ricon/X';
-import {Message, reactStyle} from 'react-atomic-molecule';
-import callfunc from 'call-func';
+import React, { PureComponent, Component } from "react";
+import PropTypes from "prop-types";
+import { Set } from "immutable";
+import Animate from "organism-react-animate";
+import XIcon from "ricon/X";
+import { Message, reactStyle } from "react-atomic-molecule";
+import callfunc from "call-func";
 
-const messageTypes = ['success', 'info', 'warning', 'error'];
+const messageTypes = ["success", "info", "warning", "error"];
 
 class Alert extends Component {
   state = {
-    hoverStyle: null,
+    hoverStyle: null
   };
 
   handleMouseEnter = () => {
     this.setState({
       hoverStyle: {
-        opacity: '.9',
-      },
+        opacity: ".9"
+      }
     });
   };
 
   handleMouseLeave = () => {
     this.setState({
-      hoverStyle: null,
+      hoverStyle: null
     });
   };
 
   handleClick = () => {
-    const {onClick} = this.props;
+    const { onClick } = this.props;
     onClick();
   };
 
   componentDidMount() {
-    const {duration, onClick} = this.props;
+    const { duration } = this.props;
     if (duration * 1 > 0) {
-      setTimeout(() => onClick(), duration);
+      setTimeout(this.handleClick, duration);
     }
   }
 
   render() {
-    const {messageType, header, message, onClick} = this.props;
-    const {hoverStyle} = this.state;
+    const { messageType, header, message, onClick } = this.props;
+    const { hoverStyle } = this.state;
     return (
       <Message messageType={messageType} header={header}>
         {message}
         <XIcon
           style={{
             ...Styles.xicon,
-            ...hoverStyle,
+            ...hoverStyle
           }}
           weight=".1rem"
           onMouseEnter={this.handleMouseEnter}
@@ -62,31 +62,31 @@ class Alert extends Component {
 
 class AlertsNotifier extends PureComponent {
   state = {
-    dismissedAlerts: Set(),
+    dismissedAlerts: Set()
   };
 
   static propTypes = {
     alerts: PropTypes.array,
-    onDismiss: PropTypes.func,
+    onDismiss: PropTypes.func
   };
 
   static defaultProps = {
     ani: {
-      appear: 'fadeIn',
-      enter: 'fadeIn',
-      leave: 'fadeOut',
+      appear: "fadeIn",
+      enter: "fadeIn",
+      leave: "fadeOut"
     },
-    position: 'top',
-    name: 'alerts',
-    duration: 3000,
+    position: "top",
+    name: "alerts",
+    duration: 5000
   };
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    const {alerts} = nextProps;
+    const { alerts } = nextProps;
     if (alerts !== prevState.prevPropsAlerts) {
       return {
         prevPropsAlerts: alerts,
-        dismissedAlerts: Set(),
+        dismissedAlerts: Set()
       };
     } else {
       return null;
@@ -94,26 +94,26 @@ class AlertsNotifier extends PureComponent {
   }
 
   dismiss(item) {
-    const {onDismiss} = this.props;
+    const { onDismiss } = this.props;
     const isContinue = callfunc(onDismiss, [item]);
     if (false !== isContinue) {
       // if no callback for dismissal, just update our state
-      this.setState(({dismissedAlerts}) => ({
-        dismissedAlerts: dismissedAlerts.add(item),
+      this.setState(({ dismissedAlerts }) => ({
+        dismissedAlerts: dismissedAlerts.add(item)
       }));
     }
   }
 
   render() {
-    const {ani, alerts, position, duration} = this.props;
-    const {dismissedAlerts} = this.state;
+    const { ani, alerts, position, duration } = this.props;
+    const { dismissedAlerts } = this.state;
     const alertArr = [];
     if (alerts && alerts.length) {
       alerts.forEach((item, key) => {
-        const thisItem = 'string' === typeof item ? {message: item} : item;
+        const thisItem = "string" === typeof item ? { message: item } : item;
         if (!dismissedAlerts.has(item)) {
           if (-1 === messageTypes.indexOf(thisItem.type)) {
-            thisItem.type = 'info';
+            thisItem.type = "info";
           }
           const oAlert = (
             <Alert
@@ -130,13 +130,13 @@ class AlertsNotifier extends PureComponent {
       });
     }
     const positionStyle = {};
-    if ('top' === position) {
+    if ("top" === position) {
       positionStyle.top = 5;
     } else {
       positionStyle.bottom = 5;
     }
     return (
-      <Animate {...ani} style={{...Styles.container, ...positionStyle}}>
+      <Animate {...ani} style={{ ...Styles.container, ...positionStyle }}>
         {alertArr}
       </Animate>
     );
@@ -147,14 +147,14 @@ export default AlertsNotifier;
 
 const Styles = {
   container: {
-    position: 'fixed',
+    position: "fixed",
     left: 10,
     right: 10,
-    zIndex: 9999,
+    zIndex: 9999
   },
   xicon: {
     top: 20,
     right: 10,
-    opacity: '.5',
-  },
+    opacity: ".5"
+  }
 };
