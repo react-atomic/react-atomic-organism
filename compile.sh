c52e80e9a0e7e24cf2be7b0ac5ec368e487f4019
Hill <hill@kimo.com>
2020-07-30 06:21:12 +0800

    update package.json style

--- compile.sh
+++ compile.sh
@@ -1,3 +1,37 @@
 #!/bin/sh
 
-npm run build
+develop(){
+    echo "Develop Mode";
+    npm run build
+}
+
+killBy(){
+    ps auxwwww | grep $1 | grep -v grep | awk '{print $2}' | xargs -I{} kill -9 {}
+}
+
+stop(){
+    DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+    killBy ${DIR}/node_modules/.bin/babel 
+}
+
+watch(){
+    stop 
+    npm run build:cjs:ui -- --watch &
+    npm run build:cjs:src -- --watch &
+    npm run build:es:ui -- --watch &
+    npm run build:es:src -- --watch &
+}
+
+case "$1" in
+  watch)
+    watch 
+    ;;
+  stop)
+    stop 
+    ;;
+  *)
+    develop
+    ;; 
+esac
+
+exit $?
