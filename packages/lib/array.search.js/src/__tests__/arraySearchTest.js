10aa21ed2eacfe8b5104217d0d0a91ed293b2581
Hill <hill@kimo.com>
2020-08-11 23:35:05 +0800

    Change to support match multiple

--- packages/lib/array.search.js/src/__tests__/arraySearchTest.js
+++ packages/lib/array.search.js/src/__tests__/arraySearchTest.js
@@ -1,36 +1,61 @@
-import {expect} from 'chai';
+import { expect } from "chai";
 
-import arraySearch from '../index';
+import arraySearch from "../index";
 
-describe('Test Array Search', () => {
-
-  it('basic test', ()=>{
+describe("Test Array Search", () => {
+  it("basic test", () => {
     const arr = [
       {},
-      {a: ['xxx']},
+      { a: ["xxx"] },
       {
-        a: 'foo',
+        a: "foo",
       },
       {
-        a: 'bar',
-      }
+        a: "bar",
+      },
     ];
-    const acture = arraySearch(arr)('a', 'BAR');
-    expect(acture).to.deep.equal([{a: 'bar'}]);
+    const acture = arraySearch(arr)({ a: "BAR" });
+    expect(acture).to.deep.equal([{ a: "bar" }]);
   });
 
-  it('test without key', ()=>{
-    const arr = ['FOO', 'BAR'];
-    const acture = arraySearch(arr)(null, 'b');
-    expect(acture).to.deep.equal(['BAR']);
+  it("test match mutiple", () => {
+    const arr = [
+      {},
+      { a: ["xxx"] },
+      {
+        a: "foo",
+      },
+      {
+        a: "bar",
+      },
+      {
+        a: "bar",
+        b: "foo",
+      },
+      {
+        a: "bar",
+        b: "foo",
+        c: "any",
+      },
+    ];
+    const acture = arraySearch(arr)({ a: "BAR", b: "Foo" });
+    expect(acture).to.deep.equal([
+      { a: "bar", b: "foo" },
+      { a: "bar", b: "foo", c: "any" },
+    ]);
   });
 
-  it('test with exact', ()=>{
-    const arr = ['a', 'aaa',  'aa']; 
-    const acture1 = arraySearch(arr)(null, 'a');
-    const acture2 = arraySearch(arr, true)(null, 'a');
-    expect(acture1.length).to.equal(3);
+  it("test without key", () => {
+    const arr = ["FOO", "BAR"];
+    const acture = arraySearch(arr)("f");
+    expect(acture).to.deep.equal(["FOO"]);
+  });
+
+  it("test with exact", () => {
+    const arr = ["a", "bbb", "aa"];
+    const acture1 = arraySearch(arr)("a");
+    const acture2 = arraySearch(arr, true)("a");
+    expect(acture1.length).to.equal(2);
     expect(acture2.length).to.equal(1);
   });
 });
-
--- packages/lib/array.search.js/src/index.js
+++ packages/lib/array.search.js/src/index.js
@@ -1,26 +1,41 @@
-import { FUNCTION } from "reshow-constant";
+import { FUNCTION, STRING } from "reshow-constant";
 
-const defaultCb = t => t + "";
+const keys = (o) => (null == o || STRING === typeof o ? [] : Object.keys(o));
+
+const defaultCb = (t) => t + "";
+
+const toArray = (maybeString) =>
+  STRING === typeof maybeString ? [maybeString] : maybeString;
 
 const keywordMatch = (haystack, needle, cb = defaultCb) =>
-  -1 !==
-  cb(haystack)
-    .toLowerCase()
-    .indexOf(cb(needle).toLowerCase());
+  keys(haystack).every((key) => {
+    return (
+      -1 !==
+      cb(haystack[key]).toLowerCase().indexOf(cb(needle[key]).toLowerCase())
+    );
+  });
 
 const exactMatch = (haystack, needle, cb = defaultCb) =>
-  cb(haystack) === cb(needle);
+  keys(haystack).every((key) => cb(haystack[key]) === cb(needle[key]));
 
-const getHaystack = (haystack, key) => (null == key ? haystack : haystack[key]);
+const getHaystack = (haystack, keyArr) => {
+  if (!keyArr.length) {
+    return [haystack];
+  } else {
+    const results = {};
+    keyArr.forEach((key) => (results[key] = haystack[key]));
+    return results;
+  }
+};
 
-const arraySearch = (arr, exact) => (key, value, cb) =>
-  (arr && arr.filter ? arr : []).filter(a => {
+const arraySearch = (arr, exact) => (needle, cb) =>
+  (arr && arr.filter ? arr : []).filter((a) => {
     if (FUNCTION === typeof exact) {
-      return exact(getHaystack(a, key), value, cb);
+      return exact(getHaystack(a, keys(needle)), toArray(needle), cb);
     } else {
       return exact
-        ? exactMatch(getHaystack(a, key), value, cb)
-        : keywordMatch(getHaystack(a, key), value, cb);
+        ? exactMatch(getHaystack(a, keys(needle)), toArray(needle), cb)
+        : keywordMatch(getHaystack(a, keys(needle)), toArray(needle), cb);
     }
   });
 
