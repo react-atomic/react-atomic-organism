89cdef574c78033340c92dc4b562305546922369
Hill <hill@kimo.com>
2020-09-04 14:42:14 +0800

    update code

--- ui/organisms/FormattedJSON.jsx
+++ ui/organisms/FormattedJSON.jsx
@@ -1,45 +1,34 @@
-import React, {Component} from 'react'; 
-import {
-    Field,
-    Form,
-    SemanticUI,
-    mixClass
-} from 'react-atomic-molecule';
+import React, { Component } from "react";
+import { Field, Form, SemanticUI, mixClass } from "react-atomic-molecule";
 
-export default class FormattedJSON extends Component
-{
-    render()
-    {
-        let outputText;
-        let outputClass;
-        const {atom, indent, children, className, ...others} = this.props;
-      try {
-        if (indent) {
-            let space = (indent === 'TAB') ? '\t' : parseInt(indent);
-            outputText = this.formatJSON(children, space);
-        } else {
-            outputText = children;
-        }
-        outputClass = 'output-good';
+class FormattedJSON extends Component {
+  render() {
+    let outputText;
+    let outputClass;
+    const { atom, indent, children, className, ...others } = this.props;
+    try {
+      if (indent) {
+        let space = indent === "TAB" ? "\t" : parseInt(indent);
+        outputText = this.formatJSON(children, space);
+      } else {
+        outputText = children;
       }
-      catch (err) {
-        // JSON.parse threw an exception
-        outputText = err.message;
-        outputClass = 'error';
-      }
+      outputClass = "output-good";
+    } catch (err) {
+      // JSON.parse threw an exception
+      outputText = err.message;
+      outputClass = "error";
+    }
 
-        let classes = mixClass(
-            className
-            ,outputClass
-        );
-        let Parent;
-        if ('form' === atom) {
-            Parent = Form;
-        } else {
-            Parent = SemanticUI;
-        }
-      return (
-        <Parent atom={atom} className="form">
+    const classes = mixClass(className, outputClass);
+    let Parent;
+    if ("form" === atom) {
+      Parent = Form;
+    } else {
+      Parent = SemanticUI;
+    }
+    return (
+      <Parent atom={atom} className="form">
         <Field
           readOnly={true}
           {...others}
@@ -47,25 +36,27 @@ export default class FormattedJSON extends Component
           value={outputText}
           className={classes}
         />
-        </Parent>
-      );
-    }
+      </Parent>
+    );
+  }
 
-    formatJSON(input, space)
-    {
-      let parsedData;
-      if (typeof input === 'string') {
-          if (input.length === 0) {
-            return '';
-          }
-          parsedData = JSON.parse(input);
-      } else {
-          parsedData = input;
+  formatJSON(input, space) {
+    let parsedData;
+    if (typeof input === "string") {
+      if (input.length === 0) {
+        return "";
       }
-      if (!parsedData) {
-        return '';
-      }
-      return JSON.stringify(parsedData, null, space);
+      parsedData = JSON.parse(input);
+    } else {
+      parsedData = input;
     }
+    if (!parsedData) {
+      return "";
+    }
+    return JSON.stringify(parsedData, null, space);
+  }
 }
-FormattedJSON.defaultProps = { atom: 'form' };
+
+FormattedJSON.defaultProps = { atom: "form" };
+
+export default FormattedJSON;
