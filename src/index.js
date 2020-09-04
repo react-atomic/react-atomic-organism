fa686d1e4323eb3cc096b98df737d8f38021f528
Hill <hill@kimo.com>
2020-09-04 13:21:29 +0800

    rename FormattedJson.jsx

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ export {default as Tappable} from '../ui/organisms/Tappable';
 // input
 export {default as Checkbox} from '../ui/organisms/Checkbox';
 export {default as RadioGroup} from '../ui/organisms/RadioGroup';
-export {default as FormattedJson} from '../ui/organisms/FormattedJson';
 export {default as SearchBox} from '../ui/organisms/SearchBox';
 export {default as Suggestion} from '../ui/organisms/Suggestion';
 export {default as OnEnterInput} from '../ui/organisms/OnEnterInput';
 export {default as ConstraintForm, ConstraintField} from '../ui/organisms/ConstraintForm';
+export {default as FormattedJSON} from '../ui/organisms/FormattedJSON';
 
 // card view
 export { default as CardView } from '../ui/organisms/CardView';
