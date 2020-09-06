8c9c7333d655e035f884db0315b4efdbc1f92864
Hill <hill@kimo.com>
2020-09-07 01:06:51 +0800

    change to reset height after post new height

--- packages/organism-react-iframe/ui/organisms/IframeContainer.jsx
+++ packages/organism-react-iframe/ui/organisms/IframeContainer.jsx
@@ -1,36 +1,39 @@
-import React, {PureComponent} from 'react';
-import get from 'get-object-value';
-import {SemanticUI} from 'react-atomic-molecule';
-import callfunc from 'call-func';
+import React, { PureComponent } from "react";
+import get from "get-object-value";
+import { SemanticUI } from "react-atomic-molecule";
+import callfunc from "call-func";
 
 let iframeCount = 0;
 
 class IframeContainer extends PureComponent {
   static defaultProps = {
-    messageKey: 'iframeH',
+    messageKey: "iframeH",
   };
 
-  state = {iframeH: 'auto'};
+  state = { iframeH: "auto" };
 
-  postHeight = win =>
-    setTimeout(() => {
-      win?.parent?.window.postMessage(
-        {
-          type: this.messageKey,
-          h: win.document.body.offsetHeight,
-        },
-        '*',
-      );
+  postHeight = (win) => {
+    this.setState({ iframeH: "auto" }, () => {
+      setTimeout(() => {
+        win?.parent?.window.postMessage(
+          {
+            type: this.messageKey,
+            h: win.document.body.offsetHeight,
+          },
+          "*"
+        );
+      });
     });
+  };
 
-  handleMessage = e => {
+  handleMessage = (e) => {
     let data = e.data;
-    if ('string' === typeof data) {
+    if ("string" === typeof data) {
       try {
-        data = JSON.parse(get(data, null, '{}'));
+        data = JSON.parse(get(data, null, "{}"));
       } catch (ex) {}
     }
-    const {type, h} = data;
+    const { type, h } = data;
     if (-1 !== `|${type}|`.indexOf(`|${this.messageKey}|`)) {
       this.setState({
         iframeH: h + 50,
@@ -40,22 +43,22 @@ class IframeContainer extends PureComponent {
 
   constructor(props) {
     super(props);
-    const {messageKey} = props;
-    this.messageKey = messageKey + '-' + iframeCount;
+    const { messageKey } = props;
+    this.messageKey = messageKey + "-" + iframeCount;
     iframeCount++;
   }
 
   componentDidMount() {
-    window.addEventListener('message', this.handleMessage, false);
+    window.addEventListener("message", this.handleMessage, false);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('message', this.handleMessage, false);
+    window.removeEventListener("message", this.handleMessage, false);
   }
 
   render() {
-    const {iframeH} = this.state;
-    const {src, refCb, style, messageKey, ...others} = this.props;
+    const { iframeH } = this.state;
+    const { src, refCb, style, messageKey, ...others } = this.props;
     if (src) {
       others.src = src;
     }
@@ -69,7 +72,7 @@ class IframeContainer extends PureComponent {
           ...style,
         }}
         atom="iframe"
-        refCb={el => {
+        refCb={(el) => {
           if (el) {
             this.iframe = el;
             callfunc(refCb, [el]);
@@ -84,7 +87,7 @@ export default IframeContainer;
 
 const Styles = {
   iframe: {
-    width: '100%',
+    width: "100%",
     border: 0,
   },
 };
