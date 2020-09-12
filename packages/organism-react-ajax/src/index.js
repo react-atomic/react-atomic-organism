6bcd969d41a0a36961291d61d8c3a3439ea2114f
Hill <hill@kimo.com>
2020-09-13 04:20:48 +0800

    let ajaxDispatch mock-able

--- packages/organism-react-ajax/src/index.js
+++ packages/organism-react-ajax/src/index.js
@@ -1,16 +1,13 @@
 // Organisms
-export { default as  AjaxLink } from '../ui/organisms/AjaxLink';
-export { default as  AjaxPage } from '../ui/organisms/AjaxPage';
-export { default as  AjaxForm } from '../ui/organisms/AjaxForm';
+export { default as AjaxLink } from "../ui/organisms/AjaxLink";
+export { default as AjaxPage } from "../ui/organisms/AjaxPage";
+export { default as AjaxForm } from "../ui/organisms/AjaxForm";
 
 // Stores
-export {
-    default as  ajaxStore,
-    initAjaxWorkerEvent
-} from './stores/ajaxStore';
+export { default as ajaxStore, initAjaxWorkerEvent } from "./stores/ajaxStore";
 
 // Dispatch
-export { ajaxDispatch } from './ajaxDispatcher';
+export { default as ajaxDispatcher, ajaxDispatch } from "./ajaxDispatcher";
 
 // Util
-export { default as  formSerialize } from 'form-serialize-js';
+export { default as formSerialize } from "form-serialize-js";
