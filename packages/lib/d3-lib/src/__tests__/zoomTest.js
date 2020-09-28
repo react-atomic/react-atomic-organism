0a609358f4e5d107738b2da06f96b059eb9fcc2d
Hill <hill@kimo.com>
2020-09-29 03:58:08 +0800

    zoom add setXYK

---
+++ packages/lib/d3-lib/src/__tests__/zoomTest.js
@@ -0,0 +1,12 @@
+import { toZoomTransform } from "../index";
+
+import { expect } from "chai";
+
+describe("Test toZoomTransform", () => {
+  it("basic test", () => {
+    const xyk = { x: 1, y: 2, k: 3 };
+    const a = toZoomTransform(xyk);
+    const {x, y, k} = a;
+    expect(xyk).to.deep.equal({x, y, k});
+  });
+});
--- packages/organism-react-graph/ui/molecules/Zoom.jsx
+++ packages/organism-react-graph/ui/molecules/Zoom.jsx
@@ -1,14 +1,10 @@
-import React, {PureComponent} from 'react';
-import {d3Zoom} from 'd3-lib';
-import callfunc from 'call-func';
+import React, { PureComponent } from "react";
+import { d3Select, d3Zoom, toZoomTransform } from "d3-lib";
+import callfunc from "call-func";
 
-import Group from '../molecules/Group';
+import Group from "../molecules/Group";
 
 class Zoom extends PureComponent {
-  static defaultProps = {
-    withTransform: true,
-  };
-
   state = {
     transform: null,
   };
@@ -16,34 +12,54 @@ class Zoom extends PureComponent {
   oD3Zoom = null;
 
   getTransform() {
-    const {transform} = this.state;
+    const { transform } = this.state;
     return transform;
+  }
+
+  setTransform(transform, e) {
+    const { onZoom, onGetEl } = this.props;
+    if (!e) {
+      e = { transform };
+      const el = d3Select(callfunc(onGetEl));
+      if (this.oD3Zoom && el) {
+        this.oD3Zoom.transform(el, transform);
+      }
+    }
+    e.zoom = this;
+    this.setState({ transform }, () => callfunc(onZoom, [e]));
+  }
+
+  setXYK({ x, y, k }) {
+    const { x: x1, y: y1, k: k1 } = this.getXYK();
+    x = x ?? x1;
+    y = y ?? y1;
+    k = k ?? k1;
+    return this.setTransform(toZoomTransform({ x, y, k }));
+  }
+
+  getXYK() {
+    const { transform } = this.state;
+    const { x, y, k } = transform || {};
+    return { x, y, k };
   }
 
   getD3Zoom = () => this.oD3Zoom;
 
   componentDidMount() {
-    const {onGetEl, onZoom} = this.props;
+    const { onGetEl } = this.props;
     setTimeout(() => {
       this.oD3Zoom = d3Zoom({
         el: callfunc(onGetEl),
         scaleExtent: [-1, 8],
-        callback: e => {
-          const {transform} = e;
-          this.setState({transform}, () => callfunc(onZoom, [e]));
-        },
+        callback: (e) => this.setTransform(e.transform, e),
       });
     });
   }
 
   render() {
-    const {onGetEl, onZoom, withTransform, ...props} = this.props;
-    const {transform} = this.state;
-    // disabe state transform, if props has will use props one
-    if (withTransform && transform) {
-      props.transform = transform+'';
-    }
-    return <Group name="zoom" {...props} />;
+    const { onGetEl, onZoom, ...props } = this.props;
+    const { transform } = this.state;
+    return <Group name="zoom" {...props} transform={transform + ""} />;
   }
 }
 
