6a19ed7e80a6a02c38d08becdd96fee9f24c0983
Hill <hill@kimo.com>
2020-11-04 22:20:02 +0800

    add IframeContainer hook support

--- packages/organism-react-iframe/ui/organisms/IframeContainer.jsx
+++ packages/organism-react-iframe/ui/organisms/IframeContainer.jsx
@@ -1,87 +1,69 @@
-import React, { PureComponent } from "react";
+import React, {
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+  forwardRef,
+} from "react";
 import get from "get-object-value";
 import { SemanticUI } from "react-atomic-molecule";
 import callfunc from "call-func";
 
 let iframeCount = 0;
 
-class IframeContainer extends PureComponent {
-  static defaultProps = {
-    messageKey: "iframeH",
-  };
-
-  state = { iframeH: "auto" };
-
-  postHeight = (win) => {
-    this.setState({ iframeH: "auto" }, () => {
+const IframeContainer = forwardRef((props, ref) => {
+  const { src, style, messageKey, ...others } = props;
+  const [iframeH, setIframeH] = useState("auto");
+  const thisMessageKey = useRef();
+  const thisStyle = useRef();
+  useImperativeHandle(ref, () => ({
+    postHeight: (win) => {
       setTimeout(() => {
         win?.parent?.window.postMessage(
           {
-            type: this.messageKey,
+            type: thisMessageKey.current,
             h: win.document.body.offsetHeight,
           },
           "*"
         );
       });
-    });
-  };
-
-  handleMessage = (e) => {
-    let data = e.data;
-    if ("string" === typeof data) {
-      try {
-        data = JSON.parse(get(data, null, "{}"));
-      } catch (ex) {}
-    }
-    const { type, h } = data;
-    if (-1 !== `|${type}|`.indexOf(`|${this.messageKey}|`)) {
-      this.setState({
-        iframeH: h + 50,
-      });
-    }
-  };
-
-  constructor(props) {
-    super(props);
-    const { messageKey } = props;
-    this.messageKey = messageKey + "-" + iframeCount;
+    },
+  }));
+  useEffect(() => {
+    thisMessageKey.current = messageKey + "-" + iframeCount;
     iframeCount++;
+    const handleMessage = (e) => {
+      let data = e.data;
+      if ("string" === typeof data) {
+        try {
+          data = JSON.parse(get(data, null, "{}"));
+        } catch (ex) {}
+      }
+      const { type, h } = data;
+      if (-1 !== `|${type}|`.indexOf(`|${thisMessageKey.current}|`)) {
+        setIframeH(h + 50);
+      }
+    };
+    window.addEventListener("message", handleMessage, false);
+    return () => {
+      window.removeEventListener("message", handleMessage, false);
+    };
+  }, []);
+  if (src) {
+    others.src = src;
   }
+  thisStyle.current = {
+    ...Styles.iframe,
+    height: iframeH,
+    minHeight: iframeH,
+    ...style,
+  };
+  return <SemanticUI {...others} style={thisStyle.current} atom="iframe" />;
+});
 
-  componentDidMount() {
-    window.addEventListener("message", this.handleMessage, false);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("message", this.handleMessage, false);
-  }
-
-  render() {
-    const { iframeH } = this.state;
-    const { src, refCb, style, messageKey, ...others } = this.props;
-    if (src) {
-      others.src = src;
-    }
-    return (
-      <SemanticUI
-        {...others}
-        style={{
-          ...Styles.iframe,
-          height: iframeH,
-          minHeight: iframeH,
-          ...style,
-        }}
-        atom="iframe"
-        refCb={(el) => {
-          if (el) {
-            this.iframe = el;
-            callfunc(refCb, [el]);
-          }
-        }}
-      />
-    );
-  }
-}
+IframeContainer.defaultProps = {
+  messageKey: "iframeH",
+};
 
 export default IframeContainer;
 
