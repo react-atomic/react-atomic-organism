dfbadb16d5f38fb656a22542e3fcd72ae3655ddd
Hill <hill@kimo.com>
2020-11-09 01:07:20 +0800

    chage to use zoom hook

--- packages/organism-react-graph/ui/molecules/Zoom.jsx
+++ packages/organism-react-graph/ui/molecules/Zoom.jsx
@@ -1,93 +1,94 @@
-import React, { PureComponent } from "react";
+import React, {
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+  forwardRef,
+} from "react";
 import { d3Select, d3Zoom, toZoomTransform } from "d3-lib";
 import callfunc from "call-func";
 
 import Group from "../molecules/Group";
 
-class Zoom extends PureComponent {
-  static defaultProps = {
-    scaleExtent: [-1, 8],
+const Zoom = forwardRef((props, ref) => {
+  const { onGetEl, onZoom, scaleExtent, ...others } = props;
+  const [transform, setTransform] = useState(null);
+  const lastEvent = useRef();
+  const lastTransform = useRef();
+  const lastD3ZoomObject = useRef();
+  const lastEnable = useRef(true);
+  const getXYK = () => {
+    const { x, y, k } = lastTransform.current || {};
+    return { x, y, k };
   };
-
-  state = {
-    transform: null,
+  const expose = {
+    getTransform: () => lastTransform.current,
+    setTransform: (transform) => handleTransform(transform),
+    getXYK,
+    setXYK: ({ x, y, k }) => {
+      const { x: x1, y: y1, k: k1 } = getXYK();
+      x = x ?? x1 ?? 0;
+      y = y ?? y1 ?? 0;
+      k = k ?? k1 ?? 1;
+      return handleTransform(toZoomTransform({ x, y, k }));
+    },
+    getD3Zoom: () => lastD3ZoomObject.current,
+    enable: () => (lastEnable.current = true),
+    disable: () => (lastEnable.current = false),
+    getEnabled: () => lastEnable.current,
   };
-
-  getTransform() {
-    const { transform } = this.state;
-    return transform;
-  }
-
-  setTransform(transform, e) {
-    const { onZoom, onGetEl } = this.props;
+  const handleTransform = (transformVal, e) => {
     if (!e) {
-      e = { transform };
-      const objD3Zoom = this.getD3Zoom();
+      e = { transform: transformVal };
+      const objD3Zoom = lastD3ZoomObject.current;
       const el = d3Select(callfunc(onGetEl));
       if (objD3Zoom && el) {
-        objD3Zoom.transform(el, transform);
+        objD3Zoom.transform(el, transformVal);
       }
     }
-    e.zoom = this;
-    this.setState({ transform }, () => callfunc(onZoom, [e]));
-  }
-
-  setXYK({ x, y, k }) {
-    const { x: x1, y: y1, k: k1 } = this.getXYK();
-    x = x ?? x1 ?? 0;
-    y = y ?? y1 ?? 0;
-    k = k ?? k1 ?? 1;
-    return this.setTransform(toZoomTransform({ x, y, k }));
-  }
-
-  getXYK() {
-    const { transform } = this.state;
-    const { x, y, k } = transform || {};
-    return { x, y, k };
-  }
-
-  componentDidMount() {
-    const { onGetEl, scaleExtent } = this.props;
-    let objD3Zoom;
-    let enableZooming = true;
+    e.zoom = expose;
+    lastEvent.current = e;
+    setTransform(transformVal);
+  };
+  useImperativeHandle(ref, () => expose);
+  useEffect(() => {
+    lastTransform.current = transform;
+    lastEvent.current && callfunc(onZoom, [lastEvent.current]);
+  }, [transform]);
+  useEffect(() => {
     let debounceTimer;
-    setTimeout(() => {
-      const el = callfunc(onGetEl);
-      objD3Zoom = d3Zoom({
-        el,
-        scaleExtent,
-        callback: (e) => {
-          debounceTimer && clearTimeout(debounceTimer);
-          debounceTimer = null;
-          if (enableZooming) {
-            this.setTransform(e.transform, e);
-          } else {
-            debounceTimer = setTimeout(() => {
-              if (
-                this.state.transform &&
-                e.transform !== this.state.transform
-              ) {
-                objD3Zoom.transform(d3Select(el), this.state.transform);
-              }
-            }, 100);
-          }
-        },
-      });
+    const el = callfunc(onGetEl);
+    lastD3ZoomObject.current = d3Zoom({
+      el,
+      scaleExtent,
+      callback: (e) => {
+        const objD3Zoom = lastD3ZoomObject.current;
+        debounceTimer && clearTimeout(debounceTimer);
+        if (lastEnable.current) {
+          return handleTransform(e.transform, e);
+        } else {
+          debounceTimer = setTimeout(() => {
+            debounceTimer = null;
+            if (
+              lastTransform.current &&
+              lastTransform.current !== e.transform
+            ) {
+              objD3Zoom.transform(d3Select(el), lastTransform.current);
+            }
+          });
+        }
+      },
     });
-    this.getD3Zoom = () => objD3Zoom;
-    this.enable = () => (enableZooming = true);
-    this.disable = () => (enableZooming = false);
-    this.getEnable = () => enableZooming;
-  }
+  }, []);
+  return (
+    <Group name="zoom" {...others} transform={transform && transform + ""} />
+  );
+});
 
-  render() {
-    const { onGetEl, onZoom, scaleExtent, ...props } = this.props;
-    const { transform } = this.state;
+Zoom.defaultProps = {
+  scaleExtent: [-1, 8],
+};
 
-    return (
-      <Group name="zoom" {...props} transform={transform && transform + ""} />
-    );
-  }
-}
+Zoom.displayName = 'Zoom';
 
 export default Zoom;
