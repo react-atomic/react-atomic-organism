b4e1bee10d6f25b9d708f0693c1b85a3fb14ffed
Hill <hill@kimo.com>
2020-11-16 22:07:15 +0800

    change to use hook

--- packages/organism-react-graph/ui/molecules/DragAndDrop.jsx
+++ packages/organism-react-graph/ui/molecules/DragAndDrop.jsx
@@ -1,4 +1,8 @@
-import React, { PureComponent } from "react";
+import React, {
+  useImperativeHandle,
+  useRef,
+  forwardRef,
+} from "react";
 import { build } from "react-atomic-molecule";
 import { d3DnD, d3Event } from "d3-lib";
 import getOffset, { unifyTouch } from "getoffset";
@@ -6,24 +10,29 @@ import get from "get-object-value";
 import callfunc from "call-func";
 import { doc } from "win-doc";
 
-class DragAndDrop extends PureComponent {
-  static defaultProps = {
-    absX: 0,
-    absY: 0,
-  };
-
-  start = {};
-  last = {};
-  el = null;
-
-  handleStart = () => {
-    const { onDragStart, zoom } = this.props;
+const DragAndDrop = forwardRef((props, ref) => {
+  const {
+    component,
+    style,
+    absX,
+    absY,
+    zoom,
+    refCb,
+    onDragStart,
+    onDrag,
+    onDragEnd,
+    ...others
+  } = props;
+  const startPoint = useRef();
+  const lastPoint = useRef();
+  const thisEl = useRef();
+  const handleStart = () => {
     const zoomK = get(callfunc(zoom), ["k"], 1);
     const { x: fromX, y: fromY, sourceEvent } = d3Event();
     const thisEvent = unifyTouch(sourceEvent);
-    const offset = getOffset(this.el);
+    const offset = getOffset(thisEl.current);
     const { left: elStartX, top: elStartY, w, h } = offset || {};
-    this.start = {
+    startPoint.current = {
       offset,
       fromX,
       fromY,
@@ -31,14 +40,13 @@ class DragAndDrop extends PureComponent {
       elStartY,
       zoomK,
     };
-    thisEvent.start = this.start;
+    thisEvent.start = startPoint.current;
     callfunc(onDragStart, [thisEvent]);
   };
 
-  handleDrag = () => {
+  const handleDrag = () => {
     const { x, y, dx, dy, sourceEvent } = d3Event();
     const thisEvent = unifyTouch(sourceEvent);
-    const { absX, absY, onDrag, zoom } = this.props;
     const zoomK = get(callfunc(zoom), ["k"], 1);
     const nextAbsX = absX + dx / zoomK;
     const nextAbsY = absY + dy / zoomK;
@@ -51,75 +59,63 @@ class DragAndDrop extends PureComponent {
     thisEvent.destTarget = destTarget;
     thisEvent.absX = nextAbsX;
     thisEvent.absY = nextAbsY;
-    this.last = thisEvent;
-    callfunc(onDrag, [thisEvent, this.start]);
+    lastPoint.current = thisEvent;
+    callfunc(onDrag, [thisEvent, startPoint.current]);
   };
 
-  handleEnd = () => {
-    const { onDragEnd } = this.props;
+  const handleEnd = () => {
     const sourceEvent = d3Event().sourceEvent;
     const thisEvent = unifyTouch(sourceEvent);
-    const offset = getOffset(this.el);
-    this.last.offset = offset;
+    const offset = getOffset(thisEl.current);
+    lastPoint.current.offset = offset;
     thisEvent.sourceEvent = sourceEvent;
-    thisEvent.last = this.last;
-    thisEvent.start = this.start;
+    thisEvent.last = lastPoint.current;
+    thisEvent.start = startPoint.current;
     callfunc(onDragEnd, [thisEvent]);
   };
 
-  getEl() {
-    return this.el;
-  }
-
-  handleElChange = (el) => {
-    if (el && (!this.el || !this.el.isSameNode(el))) {
-      this.el = el;
+  const handleElChange = (el) => {
+    if (el && (!thisEl.current || !thisEl.current.isSameNode(el))) {
+      thisEl.current = el;
       d3DnD({
         el,
-        start: this.handleStart,
-        drag: this.handleDrag,
-        end: this.handleEnd,
+        start: handleStart,
+        drag: handleDrag,
+        end: handleEnd,
       });
     }
-    return this.el;
+    return thisEl.current;
   };
 
-  render() {
-    const {
-      component,
-      style,
-      absX,
-      absY,
-      zoom,
-      refCb,
-      onDragStart,
-      onDrag,
-      onDragEnd,
-      ...props
-    } = this.props;
-    const { style: compStyle, refCb: compRefcb } = get(
-      component,
-      ["props"],
-      {}
-    );
-    props.style = {
-      ...Styles.container,
-      ...style,
-      ...compStyle,
+  useImperativeHandle(ref, () => {
+    getEl: () => {
+      return thisEl.current;
     };
-    if (refCb || compRefcb) {
-      props.refCb = (el) => {
-        this.handleElChange(el);
-        callfunc(refCb, [el]);
-        callfunc(compRefcb, [el]);
-      };
-    } else {
-      props.onGetEl = this.handleElChange;
-    }
+  });
 
-    return build(component)(props);
+  const { style: compStyle, refCb: compRefcb } = get(component, ["props"], {});
+  others.style = {
+    ...Styles.container,
+    ...style,
+    ...compStyle,
+  };
+  if (refCb || compRefcb) {
+    others.refCb = (el) => {
+      handleElChange(el);
+      callfunc(refCb, [el]);
+      callfunc(compRefcb, [el]);
+    };
+  } else {
+    others.onGetEl = handleElChange;
   }
-}
+
+  return build(component)(others);
+});
+
+DragAndDrop.defaultProps = {
+  absX: 0,
+  absY: 0,
+};
 
 export default DragAndDrop;
 
