e5cf03f72fa6ffba49ac491c38e1bfed50810fb3
Hill <hill@kimo.com>
2020-11-24 15:54:09 +0800

    export positions

--- packages/lib/get-window-offset/src/index.js
+++ packages/lib/get-window-offset/src/index.js
@@ -1,6 +1,7 @@
 export default from "./getWindowOffset";
 
 export { default as alignUI } from "./alignUI";
+export { default as positions } from "./positions";
 export { default as getPositionString } from "./getPositionString";
 export { default as isOnScreen } from "./isOnScreen";
 export { default as isFixed } from "./isFixed";
