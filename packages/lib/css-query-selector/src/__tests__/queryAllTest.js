611a228f39dcf0950a6bb1aff2ab5bf8701582bb
Hill <hill@kimo.com>
2020-11-28 01:25:17 +0800

    add unit test

---
+++ packages/lib/css-query-selector/src/__tests__/queryAllTest.js
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+import { defaultQuery as query } from "../queryFrom";
+
+describe("Test query all", () => {
+
+  after(() => (document.body.innerHTML = ""));
+
+  it("test super queryAll", () => {
+    document.body.innerHTML = `
+<main id="root"><div></div><span></span></main>
+`;
+    const acture = query.all(['main', 'div', 'span']);
+    expect(acture.length).to.be.equal(3);
+  });
+
+  it("test query nothing", () => {
+    const acture1 = query.all(null);
+    const acture2 = query.all(undefined);
+    const acture3 = query.all();
+    expect([acture1, acture2, acture3]).to.deep.equal([
+      null, undefined, undefined
+    ]);
+  });
+});
