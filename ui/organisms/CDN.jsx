b6f45b3127ae5b79784317d996dbb5b2fc77a5db
Hill <hill@kimo.com>
2021-06-06 02:44:51 +0800

    update CDN

--- ui/organisms/CDN.jsx
+++ ui/organisms/CDN.jsx
@@ -1,24 +1,47 @@
-import React, { PureComponent } from "react";
-import { Image } from "react-atomic-molecule";
+import React from "react";
+import { build, Image, SemanticUI } from "react-atomic-molecule";
 const keys = Object.keys;
 
 const stripHttpReg = /^(http)?(s)?(\:)?(\/\/)/gi;
 
-export default class CDN extends PureComponent {
-  render() {
-    const { src, ...props } = this.props;
-    const cdnProps = {};
-    keys(props).forEach((key) => {
-      if ("cdn" === key.substr(0, 3)) {
-        cdnProps[key.substr(4)] = props[key];
-        delete props[key];
-      }
-    });
-    const query = keys(cdnProps)
-      .map((key) => key + "=" + encodeURIComponent(cdnProps[key]))
-      .join("&");
-    const link = src.replace(stripHttpReg, "");
-    const url = "//i2.wp.com/" + link + "?" + query;
-    return <Image {...props} style={{ margin: "0 auto" }} src={url} />;
-  }
-}
+const CDN = (props) => {
+  const {
+    src,
+    cdnHost,
+    rawLink,
+    linkComponent,
+    linkProps,
+    ...otherProps
+  } = props;
+  const cdnProps = {};
+  keys(otherProps).forEach((key) => {
+    if ("cdn" === key.substr(0, 3)) {
+      cdnProps[key.substr(4)] = otherProps[key];
+      delete otherProps[key];
+    }
+  });
+  const query = keys(cdnProps)
+    .map((key) => key + "=" + encodeURIComponent(cdnProps[key]))
+    .join("&");
+  const cdnImgUrl = src.replace(stripHttpReg, "");
+  const url = `//${cdnHost}/${cdnImgUrl}?${query}`;
+  const linkHref = src;
+  const img = <Image {...otherProps} style={{ margin: "0 auto" }} src={url} />;
+  const result = rawLink
+    ? build(linkComponent)(
+        {
+          href: linkHref,
+          ...linkProps,
+        },
+        img
+      )
+    : img;
+  return result;
+};
+
+CDN.defaultProps = {
+  cdnHost: "i2.wp.com",
+  linkComponent: <SemanticUI atom="a" />,
+};
+
+export default CDN;
