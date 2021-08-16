1bdc023972de14de4452c9ba9ce9b493272302b9
Hill <hill@kimo.com>
2021-08-16 22:53:52 +0800

    clean log

--- packages/lib/get-window-offset/src/getDomCenter.js
+++ packages/lib/get-window-offset/src/getDomCenter.js
@@ -1,7 +1,6 @@
 import getDomPositionInfo from "./getDomPositionInfo";
 
 const calDomCenter = (left, top, width, height) => {
-  console.log({left, top, width, height});
   const xy = [
     left + Math.floor(width / 2),
     top + Math.floor(height / 2),
