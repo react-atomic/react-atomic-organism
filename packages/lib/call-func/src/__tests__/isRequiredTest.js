e07802010240ee2df274e3710d82af344b1a724d
Hill <hill@kimo.com>
2021-09-06 03:54:51 +0800

    Add isRequired

--- packages/lib/call-func/src/__tests__/isRequiredTest.js
+++ packages/lib/call-func/src/__tests__/isRequiredTest.js
@@ -1,3 +1,12 @@
 import { expect } from "chai";
 
 import { isRequired } from "../index.js";
+
+describe("Test isRequired", () => {
+  it("basic test", () => {
+    const a = (foo = isRequired("foo")) => {};
+    expect(a).to.throw("foo");
+    const b = () => a("");
+    expect(b).to.not.throw();
+  });
+});
