8299d438bd2bbbc40c7c736b86289ba188c58422
Hill <hill@kimo.com>
2021-09-08 04:36:01 +0800

    use new reshow-comstant

--- packages/lib/win-doc/src/index.js
+++ packages/lib/win-doc/src/index.js
@@ -1,16 +1,18 @@
-/**
- * !!Important!! do not use reshow-constant here
- * Bable will transpile it to double undefined
- */
+
+import {UNDEFINED} from "reshow-constant";
 
 const defaultObj = { __null: true };
 
 const doc = (w, def = defaultObj) => {
   w = w || win();
-  return "undefined" !== typeof w.document ? w.document : def;
+  return UNDEFINED !== typeof w.document ? w.document : def;
 };
 
 const win = (def = defaultObj) =>
+/**
+ * !!Important!! do not use reshow-constant here
+ * Bable will transpile it to double undefined
+ */
   "undefined" !== typeof window ? window : def;
 
 export { doc, win };
