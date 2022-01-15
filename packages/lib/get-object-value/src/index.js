0d0c39d21cca9eac5250e5564e93d6cfb5890d2e
Hill <hill@kimo.com>
2022-01-15 18:01:49 +0800

    make get-object-value more simple

--- packages/lib/get-object-value/src/index.js
+++ packages/lib/get-object-value/src/index.js
@@ -1,8 +1,9 @@
 import { DEFAULT, FUNCTION, UNDEFINED, KEYS, IS_ARRAY } from "reshow-constant";
-import toArray from "with-array";
 
 const getWebpack4Default = (o) =>
   get(o, [DEFAULT, DEFAULT], () => get(o, [DEFAULT], () => o));
+
+const getDefaultValue = (v, cur) => (FUNCTION === typeof v ? v(cur) : v ?? cur);
 
 const toJS = (v) => (v && v.toJS ? v.toJS() : v);
 
@@ -13,13 +14,8 @@ const toMap = (a, path) => {
   return nextMap;
 };
 
-const toStringForOneArray = (arr) =>
-  arr.length > 1 ? arr : arr[0] ?? undefined;
-
 const initMap = (o) => (k, defaultValue) =>
   o[k] || (o[k] = getDefaultValue(defaultValue));
-
-const getDefaultValue = (v, cur) => (FUNCTION === typeof v ? v(cur) : v ?? cur);
 
 const get = (o, path, defaultValue) => {
   if (null == o) {
@@ -32,8 +28,8 @@ const get = (o, path, defaultValue) => {
   try {
     let i = path.length;
     const j = path.length - 1;
-    while(i--) {
-      const index = path[j - i]; 
+    while (i--) {
+      const index = path[j - i];
       if (current && UNDEFINED !== typeof current[index]) {
         current = current[index];
         if (null == current) {
@@ -53,11 +49,4 @@ const get = (o, path, defaultValue) => {
 };
 
 export default get;
-export {
-  getWebpack4Default as getDefault,
-  toJS,
-  toMap,
-  toArray,
-  toStringForOneArray,
-  initMap,
-};
+export { getWebpack4Default as getDefault, toJS, toMap, initMap };
