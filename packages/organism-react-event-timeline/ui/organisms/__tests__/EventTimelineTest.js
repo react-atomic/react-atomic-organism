fdd5e51c144c6e030041e00e633d1851c7277433
Hill <hill@kimo.com>
2022-06-14 04:02:31 +0800

    organism-react-event-timeline to esm

--- packages/organism-react-event-timeline/ui/organisms/__tests__/EventTimelineTest.js
+++ packages/organism-react-event-timeline/ui/organisms/__tests__/EventTimelineTest.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { expect } from "chai";
-import { mount } from "reshow-unit";
+import { render } from "reshow-unit";
 
 import EventTimeline from "../EventTimeline";
 
 describe("EventTimeline Test", () => {
   it("basic test", () => {
-    const vDom = <EventTimeline />;
-    const wrap = mount(vDom);
-    const html = wrap.html();
-    expect(html).to.have.string("div");
+    const wrap = render(<EventTimeline />);
+    expect(wrap.html()).to.have.string("div");
   });
 });
