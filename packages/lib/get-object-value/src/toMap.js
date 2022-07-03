4caed6028e44f72a7117f3fa697aaca1f68d20dc
Hill <hill@kimo.com>
2022-07-03 20:50:43 +0800

    improve tomap

--- packages/lib/get-object-value/src/toMap.js
+++ packages/lib/get-object-value/src/toMap.js
@@ -1,10 +1,10 @@
-import { KEYS } from "reshow-constant";
+import { KEYS, NEW_OBJ } from "reshow-constant";
 import get from "./get";
 import toJS from "./toJS";
 
 const toMap = (a, path) => {
-  const next = get(a, path, {});
-  const nextMap = {};
+  const next = get(a, path, NEW_OBJ());
+  const nextMap = NEW_OBJ();
   if (next.forEach) {
     next.forEach((v, k) => (nextMap[k] = toJS(v)));
   } else {
