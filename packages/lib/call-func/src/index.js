3043dbdde4dda99ab6581028ed3702e118769e81
Hill <hill@kimo.com>
2022-07-11 03:50:15 +0800

    export callfunc keep same behavior

--- packages/lib/call-func/src/index.js
+++ packages/lib/call-func/src/index.js
@@ -1,7 +1,7 @@
 export { default as isRequired } from "./isRequired";
 export { default as throttle } from "./throttle";
 export { default as debounce } from "./debounce";
-export { default } from "./callfunc";
 export { default as defaultCall } from "./defaultCall";
 export { default as getEventKey } from "./getEventKey";
 export { register, cleanAllRegister } from "./register";
+export { default } from "./callfunc";
