d4765b211e0afb143b657e14f934bb37d62cbb2b
Hill <hill@kimo.com>
2022-08-24 19:39:11 +0800

    Add SearchButton

--- packages/organism-react-select-filter/src/index.js
+++ packages/organism-react-select-filter/src/index.js
@@ -1,6 +1,8 @@
-// Default
-export { default as CommandPalette } from "../ui/organisms/CommandPalette";
-export { default as SelectFilter } from "../ui/organisms/SelectFilter";
+// component 
+export { default as CommandPalette } from "./ui/organisms/CommandPalette";
+export { default as SelectFilter } from "./ui/organisms/SelectFilter";
+export { default as KeyButton } from "./ui/molecules/KeyButton";
+export { default as SearchButton } from "./ui/molecules/SearchButton";
 
 // hooks
 export { default as useScrollToSelect } from "./useScrollToSelect";
