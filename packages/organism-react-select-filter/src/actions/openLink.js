fa9e94bc7f5f259c9cd37c2c7c419989a6e59e13
Hill <hill@kimo.com>
2022-09-14 03:31:13 +0800

    improve jsdoc

--- packages/organism-react-select-filter/src/actions/openLink.js
+++ packages/organism-react-select-filter/src/actions/openLink.js
@@ -1,9 +1,17 @@
-import {win, doc} from "win-doc";
-const openLink = (link, bOpenToNew) => {
+// @ts-check
+
+import { win, doc } from "win-doc";
+
+/**
+ * @param {string} link
+ * @param {boolean} bOpenToNew
+ * @returns void
+ */
+const openLink = (link, bOpenToNew = false) => {
   if (bOpenToNew) {
     win().open(link, "_blank");
   } else {
-    doc().location = link; 
+    doc().location = link;
   }
 };
 
