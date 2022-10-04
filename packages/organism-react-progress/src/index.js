ed503cbe175a8ba287f460c44e382984fd030006
Hill <hill@kimo.com>
2022-10-05 03:36:31 +0800

    fixed export

--- packages/organism-react-progress/src/index.js
+++ packages/organism-react-progress/src/index.js
@@ -1,2 +1,2 @@
-export { defalut as PageLoadProgressHandler } from "./ui/organisms/PageLoadProgressHandler";
+export { default as PageLoadProgressHandler } from "./ui/organisms/PageLoadProgressHandler";
 export { default as ProgressBar } from "./ui/organisms/ProgressBar";
--- packages/organism-react-progress/types/index.d.ts
+++ packages/organism-react-progress/types/index.d.ts
@@ -1,1 +1,2 @@
+export { default as PageLoadProgressHandler } from "./ui/organisms/PageLoadProgressHandler";
 export { default as ProgressBar } from "./ui/organisms/ProgressBar";
