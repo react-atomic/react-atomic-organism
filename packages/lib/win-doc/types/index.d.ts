2622bc8e4a5c48cb1bb49d307a7e5faff29e26d4
Hill <hill@kimo.com>
2022-10-06 01:25:50 +0800

    Add types

---
+++ packages/lib/win-doc/types/index.d.ts
@@ -0,0 +1,16 @@
+/**
+ * @param {object|undefined} w
+ * @param {object} def
+ * @returns {object}
+ */
+export function doc(w?: object | undefined, def?: object): object;
+/**
+ * @param {object} def
+ * @returns {object}
+ */
+export function win(def?: object): object;
+/**
+ * @param {string} key
+ * @returns {boolean}
+ */
+export function hasWin(key?: string): boolean;
