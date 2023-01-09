711304a330ca03b8123fa941c21b36f5473653a7
Hill <hill@kimo.com>
2023-01-09 16:39:50 +0800

    fixed type script

--- packages/organism-react-ajax/types/stores/ajaxStore.d.ts
+++ packages/organism-react-ajax/types/stores/ajaxStore.d.ts
@@ -1,5 +1,14 @@
 export default store;
-declare const store: any;
+export type StateType = {
+    get: Function;
+    set: Function;
+};
+export type MaybeMapType = StateType | any;
+export type AjaxStore = {
+    urlDispatch: Function;
+};
+export type ReducerType = (state: StateType, action: MaybeMapType) => StateType;
+declare const store: import("reshow-flux-base/types/createReducer").Store & import("reshow-flux/types/ImmutableStore").ImmutableStore & AjaxStore;
 export function initAjaxWorkerEvent(worker: any): void;
 export const ajaxDispatch: (action: string | object | Function, actionParams?: object) => any;
 export function getRawUrl({ url, path, baseUrl }?: {
