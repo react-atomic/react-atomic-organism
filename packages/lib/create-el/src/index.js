62aa6b3aab697b5aa298a912baebd37af4e195d8
Hill <hill@kimo.com>
2023-02-11 23:02:27 +0800

    update typescript

--- packages/lib/create-el/src/index.js
+++ packages/lib/create-el/src/index.js
@@ -1,67 +1,96 @@
+//@ts-check
+
 import callfunc from "call-func";
 import { doc } from "win-doc";
 import { KEYS } from "reshow-constant";
 
-const inject = (base, isPrepend) => (dNode) => {
-  base = callfunc(base);
-  if (base && (base.nodeName === "BODY" || base.nodeName === "HEAD")) {
-    if (isPrepend && base.firstChild) {
-      base.insertBefore(dNode, base.firstChild);
-      return;
-    } else {
-      base.appendChild(dNode);
-      return;
-    }
-  } else {
-    const d = doc();
-    if (!base) {
-      base = d.currentScript ? d.currentScript : d.body;
-    }
-    const parentNode = base.parentNode;
-    if (parentNode) {
-      if (!isPrepend) {
-        if (base.nextSibling) {
-          parentNode.insertBefore(dNode, base.nextSibling);
-          return;
-        } else {
-          parentNode.appendChild(dNode);
-          return;
-        }
+/**
+ * @param {Element} base
+ * @param {boolean} isPrepend
+ */
+const inject =
+  (base, isPrepend) =>
+  /**
+   * @param {Element} dNode
+   */
+  (dNode) => {
+    base = callfunc(base);
+    if (base && (base.nodeName === "BODY" || base.nodeName === "HEAD")) {
+      if (isPrepend && base.firstChild) {
+        base.insertBefore(dNode, base.firstChild);
+        return;
       } else {
-        parentNode.insertBefore(dNode, base);
+        base.appendChild(dNode);
         return;
       }
-    }
-    d.body.appendChild(dNode);
-  }
-};
-
-const create = (tag) => (callback) => (attrs) => {
-  const d = doc();
-  if (d.createElement) {
-    const dNode = d.createElement(tag);
-    if (attrs) {
-      KEYS(attrs).forEach((key) => (dNode[key] = attrs[key]));
-    }
-    if (callback) {
-      let _isRun;
-      dNode.onreadystatechange = dNode.onload = () => {
-        const readyState = dNode.readyState;
-        if (
-          !readyState ||
-          -1 !== "|loaded|complete|".indexOf("|" + readyState + "|")
-        ) {
-          if (!_isRun) {
-            _isRun = true;
-            setTimeout(callback);
+    } else {
+      const d = doc();
+      if (!base) {
+        base = d.currentScript ? d.currentScript : d.body;
+      }
+      const parentNode = base.parentNode;
+      if (parentNode) {
+        if (!isPrepend) {
+          if (base.nextSibling) {
+            parentNode.insertBefore(dNode, base.nextSibling);
+            return;
+          } else {
+            parentNode.appendChild(dNode);
+            return;
           }
+        } else {
+          parentNode.insertBefore(dNode, base);
+          return;
         }
-      };
+      }
+      d.body.appendChild(dNode);
     }
-    return dNode;
-  }
-};
+  };
 
+/**
+ * @param {string} tag
+ */
+const create =
+  (tag) =>
+  /**
+   * @param {CallableFunction} callback
+   */
+  (callback) =>
+  /**
+   * @param {object} attrs
+   * @returns {Element}
+   */
+  (attrs) => {
+    const d = doc();
+    if (d.createElement) {
+      const dNode = d.createElement(tag);
+      if (attrs) {
+        KEYS(attrs).forEach((key) => (dNode[key] = attrs[key]));
+      }
+      if (callback) {
+        let _isRun;
+        const cb = () => {
+          const readyState = { readyState: null, ...dNode }.readyState;
+          if (
+            !readyState ||
+            -1 !== "|loaded|complete|".indexOf("|" + readyState + "|")
+          ) {
+            if (!_isRun) {
+              _isRun = true;
+              setTimeout(callback);
+            }
+          }
+        };
+        dNode.onload = cb;
+        dNode.addEventListener("readystatechange", cb);
+      }
+      return dNode;
+    }
+  };
+
+/**
+ * @param {Element} dNode
+ */
 const remove = (dNode) => {
   if (dNode) {
     try {
@@ -71,26 +100,70 @@ const remove = (dNode) => {
   }
 };
 
-const js = (base, isPrepend) => (callback) => (url, attrs) => {
-  const dNode = create("script")(callback)(attrs);
-  if (false !== base) {
-    inject(base, isPrepend)(dNode);
-  }
-  dNode.src = url;
-  return dNode;
-};
+/**
+ * @param {any} v
+ * @returns {HTMLScriptElement}
+ */
+const toScript = (v) => v;
 
-const css = (base, isPrepend) => (callback) => (url, attrs) => {
-  const dNode = create("link")(callback)({
-    rel: "stylesheet",
-    type: "text/css",
-    ...attrs,
-  });
-  if (false !== base) {
-    inject(base, isPrepend)(dNode);
-  }
-  dNode.href = url;
-  return dNode;
-};
+/**
+ * @param {Element} base
+ * @param {boolean} isPrepend
+ */
+const js =
+  (base, isPrepend) =>
+  /**
+   * @param {CallableFunction} callback
+   */
+  (callback) =>
+  /**
+   * @param {string} url
+   * @param {object} attrs
+   * @returns {HTMLScriptElement}
+   */
+  (url, attrs) => {
+    const oNode = create("script")(callback)(attrs);
+    if (null != base) {
+      inject(base, isPrepend)(oNode);
+    }
+    const dNode = toScript(oNode);
+    dNode.src = url;
+    return dNode;
+  };
+
+/**
+ * @param {any} v
+ * @returns {HTMLLinkElement}
+ */
+const toCss = (v) => v;
+
+/**
+ * @param {Element} base
+ * @param {boolean} isPrepend
+ */
+const css =
+  (base, isPrepend) =>
+  /**
+   * @param {CallableFunction} callback
+   */
+  (callback) =>
+  /**
+   * @param {string} url
+   * @param {object} attrs
+   * @returns {HTMLLinkElement}
+   */
+  (url, attrs) => {
+    const oNode = create("link")(callback)({
+      rel: "stylesheet",
+      type: "text/css",
+      ...attrs,
+    });
+    if (null != base) {
+      inject(base, isPrepend)(oNode);
+    }
+    const dNode = toCss(oNode);
+    dNode.href = url;
+    return dNode;
+  };
 
 export { js, css, inject, create, remove };
---
+++ packages/lib/create-el/types/index.d.ts
@@ -0,0 +1,23 @@
+/**
+ * @param {Element} base
+ * @param {boolean} isPrepend
+ */
+export function js(base: Element, isPrepend: boolean): (callback: CallableFunction) => (url: string, attrs: object) => HTMLScriptElement;
+/**
+ * @param {Element} base
+ * @param {boolean} isPrepend
+ */
+export function css(base: Element, isPrepend: boolean): (callback: CallableFunction) => (url: string, attrs: object) => HTMLLinkElement;
+/**
+ * @param {Element} base
+ * @param {boolean} isPrepend
+ */
+export function inject(base: Element, isPrepend: boolean): (dNode: Element) => void;
+/**
+ * @param {string} tag
+ */
+export function create(tag: string): (callback: CallableFunction) => (attrs: object) => Element;
+/**
+ * @param {Element} dNode
+ */
+export function remove(dNode: Element): void;
