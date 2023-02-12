212175ff1b65bd0dc9f4b2b6f3fc72dca6524730
Hill <hill@kimo.com>
2023-02-13 03:00:58 +0800

    bump new version

--- packages/lib/get-safe-reg/README.md
+++ packages/lib/get-safe-reg/README.md
@@ -1,30 +1,24 @@
-Get safe regular-expression
-===============
-   * GIT
-      * https://github.com/react-atomic/react-atomic-organism/tree/master/packages/lib/get-safe-reg
-   * NPM
-      * https://www.npmjs.com/package/get-safe-reg 
+# Get safe regular-expression
 
-## !!Important!! 
-__Wrose case on compress code with cache currying__
-* Bad (Do not use it in compresss code)
+-   GIT
+    -   https://github.com/react-atomic/react-atomic-organism/tree/master/packages/lib/get-safe-reg
+-   NPM
+    -   https://www.npmjs.com/package/get-safe-reg
+
+## !!Important!!
+
+**Wrose case on compress code with cache currying**
+
+-   Bad (Do not use it in compresss code)
+
 ```
-import getSafeReg, {cacheReg} from 'get-safe-reg';
-
-const getRegString = name => '(?:^|\\s+)'+ getSafeReg(name)+ '(?:\\s+|$)';
-
 const cache={};
-
 export default name => cacheReg(cache)(getRegString)(name);
 ```
 
-* Good
+-   Good
+
 ```
-import getSafeReg, {cacheReg} from 'get-safe-reg';
-
-const getRegString = name => '(?:^|\\s+)'+ getSafeReg(name)+ '(?:\\s+|$)';
-
 const cache = cacheReg({})(getRegString)
-
 export default name => cache(name);
 ```
--- packages/lib/get-safe-reg/src/__tests__/wildcardSearchTest.js
+++ packages/lib/get-safe-reg/src/__tests__/wildcardSearchTest.js
@@ -2,46 +2,48 @@ import { expect } from "chai";
 import { wildcardSearch } from "../index";
 
 describe("test searchRegPath", () => {
-   it("basic test", () => {
-     const actual = wildcardSearch("/user/aaa/bbb", "/user/:foo/:bar");
-     expect(actual).to.deep.equals({ foo: "aaa", bar: "bbb" });
-   });
-
-   it("test dash", () => {
-     const actual = wildcardSearch("w-1", ":key-:val");
-     expect(actual).to.deep.equals({ key: "w", val: "1" });
-   });
-
-   it("test []", () => {
-     const actual = wildcardSearch("w-[32rem]", ':key-\\[:val\\]');
-     console.log({actual});
-     expect(actual).to.deep.equals({ key: "w", val: "32rem" });
-   });
-
-   it("test ?", () => {
-     const actual1 = wildcardSearch("/a/a", "/??/:id");
-     expect(actual1).to.be.false;
-     const actual2 = wildcardSearch("/aa/b", "/??/:id");
-     expect(actual2).to.deep.equals({ id: "b" });
-     const actual3 = wildcardSearch("/aa/bb/cc", "/??/:id/??");
-     expect(actual3).to.deep.equals({ id: "bb" });
-     const actual4 = wildcardSearch("/aaa/111", "/??/:id");
-     expect(actual4).to.deep.equals({ id: "111" });
-   });
-
-   it("test *", () => {
-     const actual = wildcardSearch("/111/222/333", "/*/:id/*");
-     expect(actual).to.deep.equals({ undefined: ["111", "333"], id: "222" });
-   });
-
-   it("test * with ?", () => {
-     const actual = wildcardSearch("/abcd", "/*??");
-     expect(actual).to.deep.equals({ undefined: "ab" });
-   });
-
-  it("test number", ()=>{
-    const actual = wildcardSearch("/ddd/cccc/a.13/", "/*/a.[0-1]2");
-    console.log({actual});
+  it("basic test", () => {
+    const actual = wildcardSearch("/user/aaa/bbb", "/user/:foo/:bar");
+    expect(actual).to.deep.equals({ foo: "aaa", bar: "bbb" });
   });
 
+  it("test dash", () => {
+    const actual = wildcardSearch("w-1", ":key-:val");
+    expect(actual).to.deep.equals({ key: "w", val: "1" });
+  });
+
+  it("test ?", () => {
+    const actual1 = wildcardSearch("/a/a", "/??/:id");
+    expect(actual1).to.be.false;
+    const actual2 = wildcardSearch("/aa/b", "/??/:id");
+    expect(actual2).to.deep.equals({ id: "b" });
+    const actual3 = wildcardSearch("/aa/bb/cc", "/??/:id/??");
+    expect(actual3).to.deep.equals({ id: "bb" });
+    const actual4 = wildcardSearch("/aaa/111", "/??/:id");
+    expect(actual4).to.deep.equals({ id: "111" });
+  });
+
+  it("test *", () => {
+    const actual = wildcardSearch("/111/222/333", "/*/:id/*");
+    expect(actual).to.deep.equals({ undefined: ["111", "333"], id: "222" });
+  });
+
+  it("test * with ?", () => {
+    const actual = wildcardSearch("/abcd", "/*??");
+    expect(actual).to.deep.equals({ undefined: "ab" });
+  });
+
+  it("test []", () => {
+    const actual1 = wildcardSearch("w-[32rem]", ":key-\\[:val\\]");
+    expect(actual1).to.deep.equals({ key: "w", val: "32rem" });
+    const actual2 = wildcardSearch("w-[32rem]", ":key-[:val]", {
+      type: "bracketsEsc",
+    });
+    expect(actual2).to.deep.equals({ key: "w", val: "32rem" });
+  });
+
+  it("test number []", () => {
+    const actual = wildcardSearch("/ddd/cccc/a.13/", "/*/a.[0-1]3");
+    expect(actual).to.deep.equals({ undefined: "ddd/cccc" });
+  });
 });
