28d4f8fa85f3ff5168a0338de384b3c8d495afd3
Hill <hill@kimo.com>
2023-10-16 16:07:37 +0800

    add custom component support

--- packages/organism-react-video/src/ui/organisms/VideoFile.jsx
+++ packages/organism-react-video/src/ui/organisms/VideoFile.jsx
@@ -1,13 +1,18 @@
 // @ts-check
-import * as React from "react";
-import { SemanticUI } from "react-atomic-molecule";
 import { KEYS } from "reshow-constant";
+import build from "reshow-build";
+
+/**
+ * @typedef {import('reshow-build').Component} Component
+ */
 
 /**
  * @typedef {object} VideoFileProps
  * @property {string} src
  * @property {string} sourceType
  * @property {{[key: string]: string}} otherSources
+ * @property {Component} sourceComponent
+ * @property {Component} videoComponent
  */
 
 /**
@@ -15,20 +20,26 @@ import { KEYS } from "reshow-constant";
  *
  * sourceType: "video/mp4"
  */
-const VideoFile = ({ src, sourceType, otherSources, ...restProps }) => {
-  let thisOtherSources = null;
+const VideoFile = ({
+  src,
+  sourceType,
+  otherSources,
+  sourceComponent = "source",
+  videoComponent = "video",
+  ...restProps
+}) => {
+  const sourceEl = build(sourceComponent);
+  const videoEl = build(videoComponent);
+  const thisSources = [sourceEl({ src, type: sourceType, key: "default" })];
   if (otherSources) {
-    thisOtherSources = KEYS(otherSources).map((key) => {
-      const sSrc = otherSources[key];
-      return <SemanticUI atom="source" src={sSrc} type={key} key={key} />;
-    });
+    thisSources.push(
+      ...KEYS(otherSources).map((key) => {
+        const sSrc = otherSources[key];
+        return sourceEl({ src: sSrc, type: key, key });
+      })
+    );
   }
-  return (
-    <SemanticUI atom="video" controls {...restProps}>
-      <SemanticUI atom="source" src={src} type={sourceType} />
-      {thisOtherSources}
-    </SemanticUI>
-  );
+  return videoEl({ controls: true, ...restProps }, thisSources);
 };
 
 export default VideoFile;
--- packages/organism-react-video/types/ui/organisms/VideoFile.d.ts
+++ packages/organism-react-video/types/ui/organisms/VideoFile.d.ts
@@ -1,20 +1,28 @@
 export default VideoFile;
+export type Component = import('reshow-build').Component;
 export type VideoFileProps = {
     src: string;
     sourceType: string;
     otherSources: {
         [key: string]: string;
     };
+    sourceComponent: Component;
+    videoComponent: Component;
 };
+/**
+ * @typedef {import('reshow-build').Component} Component
+ */
 /**
  * @typedef {object} VideoFileProps
  * @property {string} src
  * @property {string} sourceType
  * @property {{[key: string]: string}} otherSources
+ * @property {Component} sourceComponent
+ * @property {Component} videoComponent
  */
 /**
  * @param {VideoFileProps} props
  *
  * sourceType: "video/mp4"
  */
-declare function VideoFile({ src, sourceType, otherSources, ...restProps }: VideoFileProps): import("react/jsx-runtime").JSX.Element;
+declare function VideoFile({ src, sourceType, otherSources, sourceComponent, videoComponent, ...restProps }: VideoFileProps): import("react").ReactElement<any, string | import("react").JSXElementConstructor<any>>;
