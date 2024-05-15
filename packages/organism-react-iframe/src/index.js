64a3d7c014db00ccbfca6d244bf5dd888a73f86e
Hill <hill@kimo.com>
2024-05-15 16:25:16 +0800

    Add expose IframeContainerExpose

--- packages/organism-react-iframe/src/index.js
+++ packages/organism-react-iframe/src/index.js
@@ -1,4 +1,8 @@
 //@ts-check
 
+/**
+ * @typedef {import("./ui/organisms/IframeContainer").IframeContainerExpose} IframeContainerExpose
+ */
+
 export { default } from "./ui/organisms/Iframe";
 export { default as IframeContainer } from "./ui/organisms/IframeContainer";
--- packages/organism-react-iframe/types/index.d.ts
+++ packages/organism-react-iframe/types/index.d.ts
@@ -1,2 +1,3 @@
 export { default } from "./ui/organisms/Iframe";
 export { default as IframeContainer } from "./ui/organisms/IframeContainer";
+export type IframeContainerExpose = import("./ui/organisms/IframeContainer").IframeContainerExpose;
