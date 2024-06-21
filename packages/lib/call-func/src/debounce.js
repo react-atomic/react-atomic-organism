7a9e2dbe0fa35a3e88708cc001d8c2a6bfd91e31
Hill <hill@kimo.com>
2024-06-21 16:44:36 +0800

    add debounce types

--- packages/lib/call-func/src/debounce.js
+++ packages/lib/call-func/src/debounce.js
@@ -1,7 +1,24 @@
+// @ts-check
 import callfunc from "./callfunc";
 
 /**
+ * @typedef {object} DebounceExecutorOption
+ * @property {number=} delay
+ * @property {any[]=} args
+ * @property {any=} scope
+ */
+
+/**
+ * @callback DebounceExecutor
+ * @param {DebounceExecutorOption=} option
+ */
+
+/**
  * only run lasttime.
+ *
+ * @param {Function} func
+ * @param {number} defaultDelay
+ * @returns {DebounceExecutor}
  */
 const debounce = (func, defaultDelay) => {
   let timer;
--- packages/lib/call-func/types/debounce.d.ts
+++ packages/lib/call-func/types/debounce.d.ts
@@ -1,5 +1,25 @@
 export default debounce;
+export type DebounceExecutorOption = {
+    delay?: number | undefined;
+    args?: any[] | undefined;
+    scope?: any | undefined;
+};
+export type DebounceExecutor = (option?: DebounceExecutorOption | undefined) => any;
+/**
+ * @typedef {object} DebounceExecutorOption
+ * @property {number=} delay
+ * @property {any[]=} args
+ * @property {any=} scope
+ */
+/**
+ * @callback DebounceExecutor
+ * @param {DebounceExecutorOption=} option
+ */
 /**
  * only run lasttime.
+ *
+ * @param {Function} func
+ * @param {number} defaultDelay
+ * @returns {DebounceExecutor}
  */
-declare function debounce(func: any, defaultDelay: any): (option: any) => void;
+declare function debounce(func: Function, defaultDelay: number): DebounceExecutor;
