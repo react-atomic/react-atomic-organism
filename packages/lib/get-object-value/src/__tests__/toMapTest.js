b2e78bf520adab6dea0304dd316f9028f37170e7
Hill <hill@kimo.com>
2024-07-22 22:29:13 +0800

    toMap add reshow-map unit test

--- packages/lib/get-object-value/src/__tests__/toMapTest.js
+++ packages/lib/get-object-value/src/__tests__/toMapTest.js
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import toMap from "../toMap";
+import { SimpleMap } from "reshow-map";
 
 describe("Test toMap", () => {
   it("test toMap", () => {
@@ -21,5 +22,12 @@ describe("Test toMap", () => {
     const map = [{ toJS: () => "foo" }, { toJS: () => "bar" }];
     const actual = toMap(map);
     expect(actual).to.deep.equal({ 0: "foo", 1: "bar" });
+  });
+
+  it("test with SimpleMap", ()=>{
+    const data = {a: "aa", b: "bb"};
+    const map = new SimpleMap(data);
+    const actual = toMap(map);
+    expect(actual).to.deep.equal(data);
   });
 });
