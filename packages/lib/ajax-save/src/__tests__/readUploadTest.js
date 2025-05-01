57614116b5796c32c47efcc0f159e4283f75e001
Hill <hill@kimo.com>
2025-05-01 18:07:35 +0800

    fixed not use jsdom File object

--- packages/lib/ajax-save/src/__tests__/readUploadTest.js
+++ packages/lib/ajax-save/src/__tests__/readUploadTest.js
@@ -3,10 +3,12 @@ import { expect } from "chai";
 import readUpload from "../readUpload";
 
 describe("Test readUpload", () => {
+  beforeEach(() => {
+    global.File = window.File;
+  });
   it("test read upload", async () => {
-    document.body.innerHTML = '<input type="file">';
-    const simFile = new File(["FOO"], "foo.txt");
-    const fileContent = await readUpload(simFile);
+    const fakeFile = new File(["FOO"], "foo.txt", { type: "text/plain" });
+    const fileContent = await readUpload(fakeFile);
     expect(fileContent).to.equal("FOO");
   });
 });
